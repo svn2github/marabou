1cdab24a0bf3d8b30e003f123e9f438ec86ac134
rudi <rudi@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2003-05-06 15:58:44 +0000

    Change color back to default, not to black
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@99 514c3d59-69d9-4452-b10b-b80f2e82d9e4

--- powerpc/readout/inc/SetColor.h
+++ powerpc/readout/inc/SetColor.h
@@ -22,7 +22,7 @@
 // Keywords:       
 //////////////////////////////////////////////////////////////////////////////
 
-static const char setblack[]	=	"\033[39m";
+static const char setblack[]	=	"\033[30m";
 static const char setred[]		=	"\033[31m";
 static const char setgreen[]	=	"\033[32m";
 static const char setyellow[]	=	"\033[33m";
