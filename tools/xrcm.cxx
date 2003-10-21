33be0f51c8fca76da7ad58ca582036d2263f3ca2
rudi <rudi@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2003-10-21 13:08:47 +0000

    Now with ACLiC mode and lib specifiers.
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@271 514c3d59-69d9-4452-b10b-b80f2e82d9e4

--- tools/xrcm.cxx
+++ tools/xrcm.cxx
@@ -15,21 +15,24 @@
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <utime.h>
 #include <string.h>
 #include <iostream.h>
+#include <fstream.h>
 #include "SetColor.h"
 
 main(int argc, char *argv[]) {
 
-	char * rootArgv[100];
-	char * rootProg;
-
 	if (argc <= 1) {
 		cerr	<< setred << endl
 				<< "xrcm: ROOT macro missing -" << endl << endl
-				<< "Usage: xrcm mfile [arg1, ..., argN]" << endl << endl
-				<< "       mfile  -- file containing ROOT macro (ext = .C)" << endl
-				<< "       argX   -- opt. arguments"
+				<< "Usage: xrcm [+|++[g|O]] [-lX1 ... -lXn] mfile [arg1, ..., argN]" << endl << endl
+				<< "       +|++ g|O   -- load macro using ACLiC method" << endl
+				<< "       -lXi       -- load libraries \"libXi.so\" (ACLiC mode only)" << endl
+				<< "       mfile      -- file containing ROOT macro (ext = .C)" << endl
+				<< "       argX       -- opt. arguments"
 				<< setblack << endl << endl;
 		exit(1);
 	}
@@ -37,7 +40,7 @@ main(int argc, char *argv[]) {
 	const char * ep = getenv("ROOTSYS");
 	if (ep == NULL || sizeof(ep) == 0) {
 		cerr	<< setred
-				<< "xrcm: env variable ROOTSYS must be set properly"
+				<< "xrcm: Env variable ROOTSYS must be set properly"
 				<< setblack << endl;
 		exit(1);
 	}
@@ -45,31 +48,110 @@ main(int argc, char *argv[]) {
 	ep = getenv("MARABOU");
 	if (ep == NULL || sizeof(ep) == 0) {
 		cerr	<< setred
-				<< "xrcm: env variable MARABOU must be set properly"
+				<< "xrcm: Env variable MARABOU must be set properly"
 				<< setblack << endl;
 		exit(1);
 	}
 
+	char * rootArgv[3];
+
 	rootArgv[0] = "root";
-	if (argc > 2) {
-		rootArgv[1] = (char *) calloc(1, 1000);
-		strcpy(rootArgv[1], argv[1]);
-		for (int i = 2; i < argc; i++) {
-			strcat(rootArgv[1], (i == 2) ? "(" : ",");
-			strcat(rootArgv[1], argv[i]);
+	int argIdx;
+
+	if (*argv[1] == '+') {
+		char aclic[50];
+		strcpy(aclic, argv[1]);
+		char * libp = aclic;
+		while (!isspace(*libp)) libp++;
+		*libp++ = '\0';
+		argIdx = 2;
+		ep = getenv("CPLUS_INCLUDE_PATH");
+		char mbIcl[200];
+		strcpy(mbIcl, getenv("MARABOU"));
+		strcat(mbIcl, "/include");
+		if (ep == NULL || sizeof(ep) == 0 || strstr(ep, mbIcl) == NULL) {
+			cerr	<< setred
+					<< "xrcm: Env variable CPLUS_INCLUDE_PATH must be set properly:" << endl
+					<< "      must contain \"" << mbIcl << "\"" << setblack << endl;
+			exit(1);
 		}
-		strcat(rootArgv[1], ")");
+		struct stat statBuf;
+		if (stat(argv[argIdx], &statBuf) == -1) {
+			cerr	<< setred
+					<< "xrcm: No such file - " << argv[argIdx] << setblack << endl;
+			exit(1);
+		}
+		char macroName[200];
+		char dotName[200];
+		char * sp = strrchr(argv[argIdx], '/');
+		if (sp == NULL) sp = argv[argIdx]; else sp++;
+		strcpy(macroName, sp);
+		strcpy(dotName, ".");
+		strcat(dotName, sp);
+		sp = strrchr(macroName, '.');
+		if (sp != NULL) *sp = '\0';
+		char sedCmd[200];
+		strcpy(sedCmd, "sed 's.^#!.//.' ");
+		strcat(sedCmd, argv[argIdx]);
+		strcat(sedCmd, " > ");
+		strcat(sedCmd, dotName);
+		system(sedCmd);
+		utimbuf timBuf;
+		timBuf.actime = statBuf.st_atime;
+		timBuf.modtime = statBuf.st_mtime;
+		utime(dotName, &timBuf);
+		ofstream macro(".xrcm", ios::out);
+		macro	<< "{" << endl
+				<< "	gROOT->Reset();" << endl;
+		while (libp = strstr(libp, "-l")) {
+			*(libp - 1) = '\0';
+			libp += 2;
+			if (sp = strstr(libp, "-l")) *(sp - 1) = '\0';
+			macro	<< "	gSystem->Load(\"lib" << libp << ".so\");" << endl;
+			if (sp) *(sp - 1) = ' ';
+		}
+		macro	<< "	gROOT->LoadMacro(\"" << dotName << aclic << "\");" << endl;
+		if (argc > argIdx + 1) {
+			macro	<< "	" << macroName;
+			for (int i = argIdx + 1; i < argc; i++) {
+				macro	<< ((i == argIdx + 1) ? "(" : ",");
+				macro	<< argv[i];
+			}
+			macro	<< ");" << endl;
+		} else {
+			macro	<< "	" << macroName << "();" << endl;
+		}
+		macro << "}" << endl;
+		macro.close();
+		rootArgv[1] = ".xrcm";
 	} else {
-		rootArgv[1] = argv[1];
+		argIdx = 1;
+		struct stat statBuf;
+		if (stat(argv[argIdx], &statBuf) == -1) {
+			cerr	<< setred
+					<< "xrcm: No such file - " << argv[argIdx] << setblack << endl;
+			exit(1);
+		}
+		if (argc > argIdx + 1) {
+			rootArgv[1] = (char *) calloc(1, 1000);
+			strcpy(rootArgv[1], argv[argIdx]);
+			for (int i = argIdx + 1; i < argc; i++) {
+				strcat(rootArgv[1], (i == argIdx + 1) ? "(" : ",");
+				strcat(rootArgv[1], argv[i]);
+			}
+			strcat(rootArgv[1], ")");
+		} else {
+			rootArgv[1] = argv[1];
+		}
 	}
 	rootArgv[2] = NULL;
 
-	rootProg = (char *) calloc(1, sizeof(getenv("ROOTSYS")) + sizeof("/bin/root") + 1);
+	char * rootProg = (char *) calloc(1, sizeof(getenv("ROOTSYS")) + sizeof("/bin/root") + 1);
 	strcpy(rootProg, getenv("ROOTSYS"));
 	strcat(rootProg, "/bin/root");
 
 	cout	<< setblue
-			<< "[xrcm: executing ROOT macro \"" << argv[1] << "\")]"
+			<< "[xrcm: executing ROOT macro \"" << argv[argIdx] << "\")]"
 			<< setblack << endl;
 
 	execvp(rootProg, rootArgv);
