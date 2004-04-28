fd3f578f775a0dd28328ce32bdfaae52f9aa0214
marabou <marabou@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2004-04-28 09:16:32 +0000

    *** empty log message ***
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@425 514c3d59-69d9-4452-b10b-b80f2e82d9e4

---
+++ build/unix/installfiles.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/tclsh
+
+# tcl script to install marabou files (libs or bins)
+# without disturbing running applications
+
+global argv
+
+# get from unix arg list:
+#   number of files to be installed
+#   destination where to install files
+#   list of file names to be installed
+
+set nofFiles [expr [llength $argv] - 1]
+set destDir [lindex $argv $nofFiles]
+set lofFiles [lrange $argv 0 [expr $nofFiles - 1]]
+
+# read current file version from file .version
+# increment version number
+# if version file doesn't exist start with version=1
+
+if {[file exists $destDir/.version]} {
+	set vf [open $destDir/.version r]
+	gets $vf version
+	set oldVersion $version
+	incr version
+	close $vf
+} else {
+	set oldVersion 0
+	set version 1
+}
+
+# copy files to destination dir, append version number
+# link main file name to new version
+# remove old files - will actually be removed as soon as last consumer stops
+
+foreach f $lofFiles {
+	set fileName [file tail $f]
+	exec cp $f $destDir/$fileName.$version 
+	exec ln -fs $destDir/$fileName.$version $destDir/$fileName
+	exec rm -f $destDir/$fileName.$oldVersion
+	puts "\[$f -> $destDir/$fileName.$version\]"
+}
+
+# save version number to file .version
+
+set vf [open $destDir/.version w]
+puts $vf $version
+close $vf
