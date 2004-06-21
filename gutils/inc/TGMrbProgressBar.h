33ff2c0238491263141f9f78024acbdc8088824e
marabou <marabou@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2004-06-21 08:30:03 +0000

    New class TGMrbProgressBar
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@485 514c3d59-69d9-4452-b10b-b80f2e82d9e4

---
+++ gutils/inc/TGMrbProgressBar.h
@@ -0,0 +1,76 @@
+#ifndef __TGMrbProgressBar_h__
+#define __TGMrbProgressBar_h__
+
+//_________________________________________________[C++ CLASS DEFINITION FILE]
+//////////////////////////////////////////////////////////////////////////////
+// Name:           gutils/inc/TGMrbProgressBar.h
+// Purpose:        Define utilities to be used with the MARaBOU GUI
+// Classes:        TGMrbProgressBar       -- a transient frame
+//                                             showing a progress bar
+// Description:    Graphic utilities for the MARaBOU GUI.
+// Author:         R. Lutter
+// Revision:       
+// Date:           
+// Keywords:
+//////////////////////////////////////////////////////////////////////////////
+
+#include "Rtypes.h"
+#include "TSystem.h"
+#include "TGWindow.h"
+#include "TGFrame.h"
+#include "TGLabel.h"
+#include "TGProgressBar.h"
+
+#include "TGTextView.h"
+#include "TMrbLogger.h"
+#include "TMrbLofNamedX.h"
+#include "TGMrbTextButton.h"
+
+//______________________________________________________[C++ CLASS DEFINITION]
+//////////////////////////////////////////////////////////////////////////////
+// Name:           TGMrbProgressBar
+// Purpose:        Display messages in a scrollable listbox
+// Description:    Extracts messages from MARaBOU's message loggin system
+//                 and shows them in a scrollable listbox widget
+// Keywords:
+//////////////////////////////////////////////////////////////////////////////
+
+class TGMrbProgressBar: public TGTransientFrame {
+
+	public:
+		enum {	kBarWidth = 400, kBarHeight = 50	};
+
+	public:
+		TGMrbProgressBar(const TGWindow * Parent, const TGWindow * Main,
+												const Char_t * Title,
+												UInt_t Width = kBarWidth,
+												const Char_t * BarColor = "blue",
+												const Char_t * BarText = NULL,
+												Bool_t WithDetail = kFALSE);
+												
+		~TGMrbProgressBar() {};				// default dtor
+
+		TGMrbProgressBar(const TGMrbProgressBar & f) : TGTransientFrame(f) {};	// default copy ctor
+
+		inline void SetRange(Float_t Min, Float_t Max) { fBar->SetRange(Min, Max); }; 
+		inline void Increment(Float_t Incr, const Char_t * Detail = NULL) {
+			fBar->Increment(Incr);
+			if (Detail && fDetail)  fDetail->SetText(Detail);
+		}; 
+		inline void SetBarColor(const Char_t * Color) { fBar->SetBarColor(Color); }; 
+
+		inline void Help() const { gSystem->Exec("kdehelp /usr/local/Marabou/doc/html/TGMrbProgressBar.html&"); };
+
+	protected:
+		TList fHeap;
+	
+		TGHProgressBar * fBar;			//! progress bar
+		TGLabel * fDetail;				//! label to show details
+		TGMrbTextButtonList * fAction;	//! action buttons
+								
+		TMrbLofNamedX fLofActions;
+		
+	ClassDef(TGMrbProgressBar, 1)		// [GraphUtils] MARaBOU's message viewer
+};
+
+#endif
---
+++ gutils/src/TGMrbProgressBar.cxx
@@ -0,0 +1,107 @@
+//__________________________________________________[C++ CLASS IMPLEMENTATION]
+//////////////////////////////////////////////////////////////////////////////
+// Name:           gutils/src/TGMrbProgressBar.cxx
+// Purpose:        MARaBOU graphic utilities: a progress bar
+// Description:    Implements class methods to show messages
+//                 in a scrollable listbox.
+// Keywords:
+// Author:         R. Lutter
+// Mailto:         <a href=mailto:rudi.lutter@physik.uni-muenchen.de>R. Lutter</a>
+// Revision:       
+// Date:           
+//
+//Begin_Html
+/*
+<img src=gutils/TGMrbProgressBar.gif>
+*/
+//End_Html
+//////////////////////////////////////////////////////////////////////////////
+
+#include "TEnv.h"
+#include "TGMrbLayout.h"
+#include "TGMrbProgressBar.h"
+
+// lifo to store objects located on heap
+#define HEAP(x)	fHeap.AddFirst((TObject *) x)
+
+ClassImp(TGMrbProgressBar)
+
+TGMrbProgressBar::TGMrbProgressBar( const TGWindow * Parent,
+										const TGWindow * Main,
+										const Char_t * Title,
+										UInt_t Width,
+										const Char_t * BarColor,
+										const Char_t * BarText,
+										Bool_t WithDetail) : TGTransientFrame(Parent, Main, Width, 0) {
+//__________________________________________________________________[C++ CTOR]
+//////////////////////////////////////////////////////////////////////////////
+// Name:           TGMrbProgressBar
+// Purpose:        Display a progress bar
+// Arguments:      TGWindow * Parent             -- parent window
+//                 TGWindow * Main               -- main window
+//                 Char_t * Title                -- title
+//                 UInt_t Width                  -- window width
+//                 Char_t * BarColor             -- fill color
+//                 Char_t * BarText              -- text to be displayed
+//                 Bool_t WithDetail             -- show detail
+// Results:        --
+// Exceptions:
+// Description:    Class constructor
+// Keywords:
+//////////////////////////////////////////////////////////////////////////////
+
+	const Char_t * font;
+	ULong_t black, white, gray, blue;
+
+//	Initialize colors
+	gClient->GetColorByName("black", black);
+	gClient->GetColorByName("lightblue2", blue);
+	gClient->GetColorByName("gray75", gray);
+	gClient->GetColorByName("white", white);
+
+//	Initialize graphic contexts
+	font = gEnv->GetValue("Gui.BoldFont", "-adobe-helvetica-medium-r-*-*-12-*-*-*-*-*-iso8859-1");
+	TGMrbLayout * buttonGC = new TGMrbLayout(font, black, gray);
+	HEAP(buttonGC);
+	TGMrbLayout * labelGC = new TGMrbLayout(font, black, gray);
+	HEAP(labelGC);
+	TGMrbLayout * frameGC = new TGMrbLayout(font, black, gray);
+	HEAP(frameGC);
+
+	if (BarText == NULL || *BarText == '\0') {
+		fBar = new TGHProgressBar(this, TGProgressBar::kStandard, Width - 20);
+		fBar->SetFillType(TGProgressBar::kBlockFill);
+	} else {
+		fBar = new TGHProgressBar(this, TGProgressBar::kFancy, Width - 20);
+		fBar->ShowPosition(kTRUE, kFALSE, BarText);
+	}
+	fBar->SetBarColor(BarColor);
+	HEAP(fBar);
+	this->AddFrame(fBar, new TGLayoutHints(kLHintsLeft | kLHintsExpandX, 0, 0, 0, 0));
+	
+	TGLayoutHints * frameLayout = new TGLayoutHints(kLHintsLeft | kLHintsExpandX);
+	HEAP(frameLayout);
+	frameGC->SetLH(frameLayout);
+
+	fDetail = NULL;
+	if (WithDetail) {
+		fDetail = new TGLabel(this, new TGString(""));
+		HEAP(fBar);
+		this->AddFrame(fDetail, frameGC->LH());
+		fDetail->SetTextJustify(kTextCenterX);
+	}
+
+	Window_t wdum;
+	Int_t ax, ay;
+	gVirtualX->TranslateCoordinates(Main->GetId(), Parent->GetId(),
+								(((TGFrame *) Main)->GetWidth() / 2 - this->GetWidth() / 2),
+								(((TGFrame *) Main)->GetHeight() / 2), ax, ay, wdum);
+	this->Move(ax, ay);
+
+	SetWindowName(Title);
+	SetIconName(Title);
+	MapSubwindows();
+	Resize(GetDefaultSize());
+	Resize(Width, 0);
+	MapWindow();
+}
