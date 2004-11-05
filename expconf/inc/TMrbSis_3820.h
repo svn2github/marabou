203e29063cc5cba9a7b947e4123b23b13d206ed3
marabou <marabou@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2004-11-05 12:26:50 +0000

    Implementation of module SIS3820
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@584 514c3d59-69d9-4452-b10b-b80f2e82d9e4

---
+++ expconf/inc/TMrbSis_3820.h
@@ -0,0 +1,71 @@
+#ifndef __TMrbSis_3820_h__
+#define __TMrbSis_3820_h__
+
+//_________________________________________________[C++ CLASS DEFINITION FILE]
+//////////////////////////////////////////////////////////////////////////////
+// Name:           expconf/inc/TMrbSis_3820.h
+// Purpose:        Define experimental configuration for MARaBOU
+// Class:          TMrbSis_3820        -- 32 chn 32 bit VME scaler
+// Description:    Class definitions to implement a configuration front-end for MARaBOU
+// Author:         R. Lutter
+// Revision:       $Id: TMrbSis_3820.h,v 1.1 2004-11-05 12:26:50 marabou Exp $       
+// Date:           
+// Keywords:
+//////////////////////////////////////////////////////////////////////////////
+
+namespace std {} using namespace std;
+
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <fstream>
+
+#include "Rtypes.h"
+#include "TSystem.h"
+#include "TObject.h"
+
+#include "TMrbVMEScaler.h"
+
+//______________________________________________________[C++ CLASS DEFINITION]
+//////////////////////////////////////////////////////////////////////////////
+// Name:           TMrbSis_3820
+// Purpose:        Define a VME scaler type SIS 3820
+// Description:    Defines a VME scaler SIS 3820 (list mode)
+// Keywords:
+//////////////////////////////////////////////////////////////////////////////
+
+class TMrbSis_3820 : public TMrbVMEScaler {
+
+	public:
+		enum				{	kSegSize		=	0x200	};
+		enum				{	kAddrMod		=	0x39	};
+
+		enum EMrbOffsets	{};
+
+	public:
+
+		TMrbSis_3820() {};  												// default ctor
+		TMrbSis_3820(const Char_t * ModuleName, UInt_t BaseAddr, Int_t FifoDepth = 1); 	// define a new scaler
+		~TMrbSis_3820() {};												// default dtor
+
+		Bool_t MakeReadoutCode(ofstream & RdoStrm, TMrbConfig::EMrbModuleTag TagIndex);  	// generate part of code
+		Bool_t MakeReadoutCode(ofstream & RdoStrm, TMrbConfig::EMrbModuleTag TagIndex, TObject * Channel, Int_t Value = 0);  	// generate code for given channel
+
+		virtual inline const Char_t * GetMnemonic() const { return("sis_3820"); }; 	// module mnemonic
+
+		inline void SetFifoDepth(Int_t FifoDepth) { fFifoDepth = FifoDepth; };
+		inline Int_t GetFifoDepth() const { return(fFifoDepth); };
+
+		inline void Help() const { gSystem->Exec("kdehelp /usr/local/Marabou/doc/html/TMrbSis_3820.html&"); };
+
+	protected:
+		void DefineRegisters(); 							// define vme registers
+
+	protected:
+		Int_t fFifoDepth;			// fifo depth per channel
+
+	ClassDef(TMrbSis_3820, 1)		// [Config] SIS 3820, 32 x 32 bit VME scaler
+};
+
+#endif
---
+++ expconf/src/TMrbSis_3820.cxx
@@ -0,0 +1,272 @@
+//__________________________________________________[C++ CLASS IMPLEMENTATION]
+//////////////////////////////////////////////////////////////////////////////
+// Name:           expconf/src/TMrbSis_3820.cxx
+// Purpose:        MARaBOU configuration: SIS modules
+// Description:    Implements class methods to handle a SIS scaler type 3820 
+// Keywords:
+// Author:         R. Lutter
+// Mailto:         <a href=mailto:rudi.lutter@physik.uni-muenchen.de>R. Lutter</a>
+// Revision:       $Id: TMrbSis_3820.cxx,v 1.1 2004-11-05 12:26:50 marabou Exp $       
+// Date:           
+//////////////////////////////////////////////////////////////////////////////
+
+namespace std {} using namespace std;
+
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <fstream>
+
+#include "TDirectory.h"
+
+#include "TMrbLogger.h"
+#include "TMrbConfig.h"
+#include "TMrbVMEChannel.h"
+#include "TMrbSis_3820.h"
+
+#include "SetColor.h"
+
+extern TMrbConfig * gMrbConfig;
+extern TMrbLogger * gMrbLog;
+
+ClassImp(TMrbSis_3820)
+
+TMrbSis_3820::TMrbSis_3820(const Char_t * ModuleName, UInt_t BaseAddr, Int_t FifoDepth) :
+									TMrbVMEScaler(ModuleName, "Sis_3820", BaseAddr,
+																TMrbSis_3820::kAddrMod,
+																TMrbSis_3820::kSegSize,
+																1, 32 * FifoDepth, 1 << 31) {
+//__________________________________________________________________[C++ CTOR]
+//////////////////////////////////////////////////////////////////////////////
+// Name:           TMrbSis_3820
+// Purpose:        Create a scaler of type SIS 3820
+// Arguments:      Char_t * ModuleName      -- name of camac module
+//                 UInt_t BaseAddr          -- base addr
+// Results:        --
+// Exceptions:
+// Description:    Creates a vme scaler of type SIS 3820.
+// Keywords:
+//////////////////////////////////////////////////////////////////////////////
+
+	TString codeFile;
+	UInt_t mTypeBits;
+	TString mType;
+
+	if (gMrbLog == NULL) gMrbLog = new TMrbLogger();
+	
+	if (!this->IsZombie()) {
+		if (gMrbConfig == NULL) {
+			gMrbLog->Err() << "No config defined" << endl;
+			gMrbLog->Flush(this->ClassName());
+			this->MakeZombie();
+		} else if (gMrbConfig->FindModule(ModuleName)) {
+			gMrbLog->Err() << ModuleName << ": Module name already in use" << endl;
+			gMrbLog->Flush(this->ClassName());
+			this->MakeZombie();
+		} else if (gMrbConfig->CheckModuleAddress(this)) {
+			SetTitle("SIS 3820 scaler 32 x 32 bit"); 	// store module type
+			codeFile = fModuleID.GetName();
+			codeFile += ".code";
+			if (LoadCodeTemplates(codeFile)) {
+				DefineRegisters();
+				mTypeBits = TMrbConfig::kModuleVME | TMrbConfig::kModuleListMode | TMrbConfig::kModuleScaler;
+				gMrbConfig->GetLofModuleTypes()->Pattern2String(mType, mTypeBits);
+				fModuleType.Set(mTypeBits, mType.Data());
+				fDataType = gMrbConfig->GetLofDataTypes()->FindByIndex(TMrbConfig::kDataUInt);
+				fNofShortsPerChannel = 2;
+				fFifoDepth = FifoDepth; 		// fifo depth per channel
+				fBlockReadout = kTRUE;			// module has block readout
+				gMrbConfig->AddModule(this);				// append to list of modules
+				gMrbConfig->AddScaler(this);				// and to list of scalers
+				gDirectory->Append(this);
+			} else {
+				this->MakeZombie();
+			}
+		} else {
+			this->MakeZombie();
+		}
+	}
+}
+
+void TMrbSis_3820::DefineRegisters() {
+//________________________________________________________________[C++ METHOD]
+//////////////////////////////////////////////////////////////////////////////
+// Name:           TMrbSis_3820::DefineRegisters
+// Purpose:        Define camac registers
+// Arguments:      --
+// Results:        --
+// Exceptions:
+// Description:    Defines status, common threshold, lower & upper thresholds, and offset.
+// Keywords:
+//////////////////////////////////////////////////////////////////////////////
+
+}
+
+Bool_t TMrbSis_3820::MakeReadoutCode(ofstream & RdoStrm, TMrbConfig::EMrbModuleTag TagIndex) {
+//________________________________________________________________[C++ METHOD]
+//////////////////////////////////////////////////////////////////////////////
+// Name:           TMrbSis_3820::MakeReadoutCode
+// Purpose:        Write a piece of code for a SIS scaler
+// Arguments:      ofstream & RdoStrm         -- file output stream
+//                 EMrbModuleTag TagIndex     -- index of tag word taken from template file
+// Results:        kTRUE/kFALSE
+// Exceptions:
+// Description:    Writes code for readout of a SIS 3820 scaler module.
+// Keywords:
+//////////////////////////////////////////////////////////////////////////////
+
+	TString mnemoLC, mnemoUC;
+
+	if (!fCodeTemplates.FindCode(TagIndex)) {
+		gMrbLog->Err()	<< "No code loaded for tag "
+						<< gMrbConfig->fLofModuleTags.FindByIndex(TagIndex)->GetName() << endl;
+		gMrbLog->Flush(this->ClassName(), "MakeReadoutCode");
+		return(kFALSE);
+	}
+
+	mnemoLC = this->GetMnemonic();
+	mnemoUC = mnemoLC;
+	mnemoUC.ToUpper();
+
+	switch (TagIndex) {
+		case TMrbConfig::kModuleDefs:
+		case TMrbConfig::kModuleInitCommonCode:
+			fCodeTemplates.InitializeCode();
+			fCodeTemplates.Substitute("$moduleName", this->GetName());
+			fCodeTemplates.Substitute("$moduleTitle", this->GetTitle());
+			fCodeTemplates.Substitute("$modulePosition", this->GetPosition());
+			fCodeTemplates.Substitute("$mnemoLC", mnemoLC);
+			fCodeTemplates.Substitute("$mnemoUC", mnemoUC);
+			fCodeTemplates.Substitute("$baseAddr", (Int_t) this->GetBaseAddr(), 16);
+			fCodeTemplates.WriteCode(RdoStrm);
+			break;
+		case TMrbConfig::kModuleInitModule:
+			{
+				fCodeTemplates.InitializeCode();
+				fCodeTemplates.Substitute("$moduleName", this->GetName());
+				fCodeTemplates.Substitute("$moduleTitle", this->GetTitle());
+				fCodeTemplates.Substitute("$modulePosition", this->GetPosition());
+				fCodeTemplates.Substitute("$mnemoLC", mnemoLC);
+				fCodeTemplates.Substitute("$mnemoUC", mnemoUC);
+				fCodeTemplates.Substitute("$baseAddr", (Int_t) this->GetBaseAddr(), 16);
+				Int_t pat;
+				if (this->GetNofChannelsUsed() < 32) {
+					if (this->CheckIfPatternIsContiguous()) {
+						pat = ~(this->GetPatternOfChannelsUsed());
+					} else {
+						pat = 0;
+						gMrbLog->Err()	<< "Pattern of channels used is not contiguous - 0x"
+										<< setbase(16) << this->GetPatternOfChannelsUsed() << setbase(10)
+										<< " (ignored: using 32 chns)" << endl;
+						gMrbLog->Flush(this->ClassName(), "MakeReadoutCode");
+					}
+				} else {
+					pat = 0;
+				}
+				fCodeTemplates.Substitute("$unusedChannels", pat, 16);
+				fCodeTemplates.WriteCode(RdoStrm);
+			}
+			break;
+		case TMrbConfig::kModuleClearModule:
+		case TMrbConfig::kModuleFinishReadout:
+		case TMrbConfig::kModuleStartAcquisition:
+		case TMrbConfig::kModuleStopAcquisition:
+		case TMrbConfig::kModuleStartAcquisitionGroup:
+		case TMrbConfig::kModuleStopAcquisitionGroup:
+		case TMrbConfig::kModuleUtilities:
+		case TMrbConfig::kModuleDefineGlobals:
+		case TMrbConfig::kModuleDefineLocalVarsInit:
+		case TMrbConfig::kModuleDefineLocalVarsReadout:
+		case TMrbConfig::kModuleDefinePrototypes:
+			fCodeTemplates.InitializeCode();
+			fCodeTemplates.Substitute("$moduleName", this->GetName());
+			fCodeTemplates.Substitute("$nofParams", this->GetNofChannelsUsed());
+			fCodeTemplates.Substitute("$mnemoLC", mnemoLC);
+			fCodeTemplates.Substitute("$mnemoUC", mnemoUC);
+			fCodeTemplates.WriteCode(RdoStrm);
+			break;
+		case TMrbConfig::kModuleReadModule:
+			{
+				Int_t nofChannels = this->GetNofChannelsUsed();
+				if (nofChannels < 32) {
+					if (!this->CheckIfPatternIsContiguous()) {
+						gMrbLog->Err()	<< "Pattern of channels used is not contiguous - 0x"
+										<< setbase(16) << this->GetPatternOfChannelsUsed() << setbase(10)
+										<< " (ignored: using 32 chns)" << endl;
+						gMrbLog->Flush(this->ClassName(), "MakeReadoutCode");
+						nofChannels = 32;
+					}
+				}
+				TString iniTag;
+				iniTag = (fFifoDepth > 1 ? "%MH" : "%SH");
+				iniTag += (nofChannels < 32) ? "S%" : "%";
+				fCodeTemplates.InitializeCode(iniTag.Data());
+				fCodeTemplates.Substitute("$moduleName", this->GetName());
+				fCodeTemplates.Substitute("$fifoDepth", fFifoDepth);
+				fCodeTemplates.Substitute("$nofChannels", nofChannels);
+				fCodeTemplates.Substitute("$mnemoLC", mnemoLC);
+				fCodeTemplates.Substitute("$mnemoUC", mnemoUC);
+				fCodeTemplates.WriteCode(RdoStrm);
+			}
+			break;
+	}
+	return(kTRUE);
+}
+
+
+Bool_t TMrbSis_3820::MakeReadoutCode(ofstream & RdoStrm,	TMrbConfig::EMrbModuleTag TagIndex,
+															TObject * Channel,
+															Int_t Value) {
+//________________________________________________________________[C++ METHOD]
+//////////////////////////////////////////////////////////////////////////////
+// Name:           TMrbSis_3820::MakeReadoutCode
+// Purpose:        Write a piece of code for a SIS scaler
+// Arguments:      ofstream & RdoStrm           -- file output stream
+//                 EMrbModuleTag TagIndex       -- index of tag word taken from template file
+//                 TObject * Channel            -- channel
+//                 Int_t Value                  -- value to be set
+// Results:        kTRUE/kFALSE
+// Exceptions:
+// Description:    Writes code for readout of a SIS 3820 scaler module.
+// Keywords:
+//////////////////////////////////////////////////////////////////////////////
+
+	TMrbVMEChannel * chn;
+	TString mnemoLC, mnemoUC;
+
+	chn = (TMrbVMEChannel *) Channel;
+
+	if (!fCodeTemplates.FindCode(TagIndex)) {
+		gMrbLog->Err()	<< "No code loaded for tag "
+						<< gMrbConfig->fLofModuleTags.FindByIndex(TagIndex)->GetName() << endl;
+		gMrbLog->Flush(this->ClassName(), "MakeReadoutCode");
+		return(kFALSE);
+	}
+
+	mnemoLC = this->GetMnemonic();
+	mnemoUC = mnemoLC;
+	mnemoUC.ToUpper();
+
+	switch (TagIndex) {
+		case TMrbConfig::kModuleInitChannel:
+			break;
+		case TMrbConfig::kModuleWriteSubaddr:
+			break;
+		case TMrbConfig::kModuleSetupReadout:
+		case TMrbConfig::kModuleReadChannel:
+		case TMrbConfig::kModuleIncrementChannel:
+		case TMrbConfig::kModuleSkipChannels:
+		case TMrbConfig::kModuleStoreData:
+			fCodeTemplates.InitializeCode();
+			fCodeTemplates.Substitute("$moduleName", this->GetName());
+			fCodeTemplates.Substitute("$mnemoLC", mnemoLC);
+			fCodeTemplates.Substitute("$mnemoUC", mnemoUC);
+			fCodeTemplates.Substitute("$chnName", chn->GetName());
+			fCodeTemplates.Substitute("$chnNo", chn->GetAddr());
+			fCodeTemplates.Substitute("$data", Value);
+			fCodeTemplates.WriteCode(RdoStrm);
+			break;
+	}
+	return(kTRUE);
+}
