60e7b6fc68f217c6b4d402e9c6cb107c8c1579b4
marabou <marabou@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2004-11-23 08:24:31 +0000

    Arg#2: BaseAddr
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@615 514c3d59-69d9-4452-b10b-b80f2e82d9e4

--- powerpc/readout/src/cc32.c
+++ powerpc/readout/src/cc32.c
@@ -18,12 +18,10 @@
 #include "s_evhe.h"
 #include "s_veshe.h"
 #include "cc32_camio.h"
-#include "vmelib.h"
+#include "ces/vmelib.h"
 
 #include "err_mask_def.h"
 #include "errnum_def.h"
-
-static struct pdparam_master s_param; 		/* vme segment params */
 
 extern volatile unsigned long * cc32_baseAddr;
 
@@ -37,6 +35,7 @@ int main(int argc, char *argv[]) {
 	int version;
 	unsigned short srval;
 	unsigned long bcval;
+	unsigned long * addr;
 
 	if (argc <= 1) {
 		fprintf(stderr, "cc32: Read status of CC32\nUsage: cc32 <n>   (n = number of crates)\n");
@@ -44,13 +43,15 @@ int main(int argc, char *argv[]) {
 	}
 	nofCrates = atoi(argv[1]);
 
-	s_param.iack = 1;						/* prepare vme segment */
- 	s_param.rdpref = 0;
- 	s_param.wrpost = 0;
- 	s_param.swap = SINGLE_AUTO_SWAP;
- 	s_param.dum[0] = 0; 					/* forces static mapping! */
-
-	cc32_init((unsigned long *) 0xee550000, nofCrates);
+	if (argc == 3) {
+		addr = (unsigned long *) strtol(argv[2], (char **)NULL, 16);
+	} else {
+		addr = (unsigned long *) 0xee550000;
+	}
+	
+	printf("cc32: nofCrates=%d, baseAddr=%#lx\n", nofCrates, addr);
+	
+	cc32_init(addr, nofCrates);
 	for (i = 1; i <= nofCrates; i++) {
 		sr = CC32_SET_BASE(C(i), N(0));
 		srval = CC32_READ_R2B(sr, F(0), A(0));
