b198bfe0cc9ab2548c3491fa16717d2d01533349
rudi <rudi@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2004-12-14 12:02:22 +0000

    *** empty log message ***
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@644 514c3d59-69d9-4452-b10b-b80f2e82d9e4

--- utils/src/TMrbWildcard.cxx
+++ utils/src/TMrbWildcard.cxx
@@ -7,7 +7,7 @@
 // Keywords:
 // Author:         R. Lutter
 // Mailto:         <a href=mailto:rudi.lutter@physik.uni-muenchen.de>R. Lutter</a>
-// Revision:       $Id: TMrbWildcard.cxx,v 1.1 2004-12-14 11:46:51 rudi Exp $       
+// Revision:       $Id: TMrbWildcard.cxx,v 1.2 2004-12-14 12:02:22 rudi Exp $       
 // Date:           
 //////////////////////////////////////////////////////////////////////////////
 
@@ -32,30 +32,22 @@ Bool_t TMrbWildcard::Match(const Char_t * String) const {
 
 	const Char_t * wp = fWildcard.Data();
 	const Char_t * sp = String;
-	cout << "@@----- " << sp << " ------------------------" << endl;
 	while (*wp && *sp) {
-		cout << "@@ *wp=" << *wp << " *sp=" << *sp << endl;
 		if (*wp == *sp) {
 			sp++;
 			wp++;
-			cout << "@@ sp++, wp++" << endl;
 		} else if (*wp == '?' && *sp != '/') {
 			sp++;
 			wp++;
-			cout << "@@ sp++, wp++" << endl;
 		} else if (*wp == '*') {
 			wp++;
-			cout << "@@ *wp=" << *wp << endl;
 			while (*sp && (*sp != *wp) && (*sp != '/')) {
-				cout << "@@ skipping *sp=" << *sp << endl;
 				sp++;
 			}
 		} else if (*wp != *sp) {
-			cout << "@@ no match" << endl;
 			return(kFALSE);
 		}
 	}
 	Bool_t match = (*wp == '\0') && (*sp == '\0');
-	cout << (match ? "@@ match" : "@@ no match") << endl;
 	return(match);
 }
