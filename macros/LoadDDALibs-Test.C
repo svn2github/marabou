7e4436eda40a3bdf946bbfb7b53953d8977e1ee0
Rudolf.Lutter <Rudolf.Lutter@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2005-12-06 07:41:43 +0000

    Macros Load<XXX>-Test.C no longer needed.
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@947 514c3d59-69d9-4452-b10b-b80f2e82d9e4

--- macros/LoadDDALibs-Test.C
+++
@@ -1,7 +0,0 @@
-{
-	cout << "[Loading *LOCAL* SNAKE libs from $LMARABOU/lib]" << endl;
-	gSystem->Load("$LMARABOU/lib/libTMrbUtils.so");
-	gSystem->Load("$ROOTSYS/lib/libGed.so");
-	gSystem->Load("$LMARABOU/lib/libTGMrbUtils.so");
-	gSystem->Load("$LMARABOU/lib/libTSnkDDA0816.so");
-}
--- macros/LoadDGFLibs-Test.C
+++
@@ -1,7 +0,0 @@
-{
-	cout << "[Loading *LOCAL* DGF libs from $LMARABOU/lib]" << endl;
-	gSystem->Load("$LMARABOU/lib/libTMrbUtils.so");
-	gSystem->Load("$LMARABOU/lib/libEsoneClient.so");
-	gSystem->Load("$LMARABOU/lib/libTMrbEsone.so");
-	gSystem->Load("$LMARABOU/lib/libTMrbDGF.so");
-}
