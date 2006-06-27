eb82067f7afdd3e35ecf5f4582d71dc904ac3fe6
Otto.Schaile <Otto.Schaile@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2006-06-27 12:26:54 +0000

    repair TGMrbValuesAndText
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@1014 514c3d59-69d9-4452-b10b-b80f2e82d9e4

--- gutils/inc/TGMrbValuesAndText.h
+++ gutils/inc/TGMrbValuesAndText.h
@@ -16,9 +16,10 @@
 Bool_t  GetStringExt(const char *prompt, TString * text, 
                              Int_t win_width, TGWindow *win = 0, 
                              const char * FileName = 0, TList * complist=0,
-                             TList * rowlabs =0 , TList * values = 0,
+                             TList * rowlabs =0 , void ** val_pointers=0,
                              TArrayI * Flags = 0, const char * Flagslabel = 0,
-                             const char *helptext = 0);
+                             const char *helptext = 0, TObject * calling_class = 0, 
+                             const char * cname = 0);
 //_________________________________________________________________________________________
 
 class TGMrbValuesAndText : public TGTransientFrame {
@@ -29,7 +30,8 @@ private:
    TList            *fWidgets;   // keep track of widgets to be deleted in dtor
    TString          *fText;      // address to store return string
    Int_t            *fReturn;    // address to store cancel = -1 / ok =0 
-   TList            *fValues;
+   TList            *fLabels;
+   void            **fValPointers;
    TArrayI          *fFlags;
    TList            *fFlagButtons;
    TGListBox        *fListBox;
@@ -51,21 +53,26 @@ private:
    Style_t            fMarker;
    Style_t            fPattern;
    TList				  *fEntries;
-
+   TGGC              fRedTextGC;
+   TGGC              fBlueTextGC;
+//   Int_t             fFinis; 
 public:
    TGMrbValuesAndText(const char *prompt, TString * text, 
                              Int_t * ok, Int_t win_width, const TGWindow *win=0, 
                              const char * FileName = 0, TList * complist=0,
-                             TList * rowlabs = 0, TList * values = 0,
+                             TList * rowlabs = 0, void **val_pointers = 0,
                              TArrayI * Flags = 0, const char * Flagslabel = 0,
-                             const char *helptext=0);
+                             const char *helptext=0, TObject * calling_class = 0, 
+                             const char * cname = 0);
    virtual ~TGMrbValuesAndText();
 
    virtual Bool_t  ProcessMessage(Long_t msg, Long_t parm1, Long_t parm2);
    void StoreValues();
+   void ReloadValues();
    void CloseWindow();
 //   void DoMarkerStyle(Style_t marker) {fMarker = marker;};
    void SaveList();
+   Int_t GetColorPixelByInd(Int_t index);
 
 ClassDef(TGMrbValuesAndText,0)		// [GraphUtils] A dialog box
 };   
