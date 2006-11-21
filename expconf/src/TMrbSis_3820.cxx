d71a865ef61b7eebc7500c6497c057f0e16166f4
Rudolf.Lutter <Rudolf.Lutter@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2006-11-21 14:39:49 +0000

    *** empty log message ***
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@1211 514c3d59-69d9-4452-b10b-b80f2e82d9e4

--- expconf/src/TMrbSis_3820.cxx
+++ expconf/src/TMrbSis_3820.cxx
@@ -6,7 +6,7 @@
 // Keywords:
 // Author:         R. Lutter
 // Mailto:         <a href=mailto:rudi.lutter@physik.uni-muenchen.de>R. Lutter</a>
-// Revision:       $Id: TMrbSis_3820.cxx,v 1.9 2006-11-21 14:35:23 Rudolf.Lutter Exp $       
+// Revision:       $Id: TMrbSis_3820.cxx,v 1.10 2006-11-21 14:37:28 Rudolf.Lutter Exp $       
 // Date:           
 //////////////////////////////////////////////////////////////////////////////
 
@@ -190,8 +190,8 @@ Bool_t TMrbSis_3820::MakeReadoutCode(ofstream & RdoStrm, TMrbConfig::EMrbModuleT
 				fCodeTemplates.Substitute("$inputMode", (Int_t) this->Get("InputMode"));
 				fCodeTemplates.Substitute("$outputMode", (Int_t) this->Get("OutputMode"));
 				fCodeTemplates.Substitute("$lneSource", (Int_t) this->Get("LNESource"));
-				fCodeTemplates.Substitute("$clearingMode", this->NonClearingMode() ? "NON_CLEARING_MODE" : "CLEAR_ON_COPY");
-				fCodeTemplates.Substitute("$DataFormat", this->DataFormat24() ? 0x4 : 0x0);
+				fCodeTemplates.Substitute("$clearOnCopy", this->NonClearingMode() ? "NON_CLEARING_MODE" : "CLEAR_ON_COPY");
+				fCodeTemplates.Substitute("$dataFormat", this->DataFormat24() ? 24 : 32);
 				Int_t pat;
 				if (this->GetNofChannelsUsed() < 32) {
 					if (this->CheckIfPatternIsContiguous()) {
