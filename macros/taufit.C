939afe04aedb62eb6fb909b3f6459f611ae6ec01
Rudolf.Lutter <Rudolf.Lutter@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2007-07-27 11:36:40 +0000

    *** empty log message ***
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@1361 514c3d59-69d9-4452-b10b-b80f2e82d9e4

--- macros/taufit.C
+++ macros/taufit.C
@@ -1,15 +1,17 @@
 //________________________________________________________________[ROOT MACRO]
 //////////////////////////////////////////////////////////////////////////////
 // Name:             taufit.C
-// Purpose:          Fit given data with an exp function
+// Purpose:          Taufit
 // Syntax:           .x taufit.C(const Char_t * File,
-//                             Int_t NofTaus,
-//                             Double_t A0,
-//                             Double_t Tau0,
-//                             Double_t A1,
-//                             Double_t Tau1)
-// Arguments:        Char_t * File             -- file (.dat)
-//                   Int_t NofTaus             -- number of exp function to be fitted
+//                                Int_t NofTaus,
+//                                Double_t c,
+//                                Double_t A0,
+//                                Double_t Tau0,
+//                                Double_t A1,
+//                                Double_t Tau1)
+// Arguments:        Char_t * File             -- File (.dat)
+//                   Int_t NofTaus             -- Number of exp() functions
+//                   Double_t c                -- Constant
 //                   Double_t A0               -- a0
 //                   Double_t Tau0             -- tau0
 //                   Double_t A1               -- a1
@@ -18,8 +20,8 @@
 // Author:           Rudolf.Lutter
 // Mail:             Rudolf.Lutter@lmu.de
 // URL:              www.bl.physik.uni-muenchen.de/~Rudolf.Lutter
-// Revision:         $Id: taufit.C,v 1.2 2007-07-27 11:08:27 Rudolf.Lutter Exp $
-// Date:             Wed Jul 25 15:41:44 2007
+// Revision:         $Id: taufit.C,v 1.3 2007-07-27 11:36:40 Rudolf.Lutter Exp $
+// Date:             Fri Jul 27 13:25:50 2007
 //+Exec __________________________________________________[ROOT MACRO BROWSER]
 //                   Name:                taufit.C
 //                   Title:               Taufit
@@ -27,7 +29,7 @@
 //                   Aclic:               +g
 //                   Modify:              
 //                   RcFile:              
-//                   NofArgs:             6
+//                   NofArgs:             7
 //                   Arg1.Name:           File
 //                   Arg1.Title:          File (.dat)
 //                   Arg1.Type:           Char_t *
@@ -45,38 +47,45 @@
 //                   Arg2.AddLofValues:   No
 //                   Arg2.Base:           dec
 //                   Arg2.Orientation:    horizontal
-//                   Arg3.Name:           A0
-//                   Arg3.Title:          a0
+//                   Arg3.Name:           c
+//                   Arg3.Title:          Constant
 //                   Arg3.Type:           Double_t
 //                   Arg3.EntryType:      Entry-C
-//                   Arg3.Default:        1.0
 //                   Arg3.AddLofValues:   No
 //                   Arg3.Base:           dec
 //                   Arg3.Orientation:    horizontal
-//                   Arg4.Name:           Tau0
-//                   Arg4.Title:          tau0
+//                   Arg4.Name:           A0
+//                   Arg4.Title:          a0
 //                   Arg4.Type:           Double_t
 //                   Arg4.EntryType:      Entry-C
 //                   Arg4.Default:        1.0
 //                   Arg4.AddLofValues:   No
 //                   Arg4.Base:           dec
 //                   Arg4.Orientation:    horizontal
-//                   Arg5.Name:           A1
-//                   Arg5.Title:          a1
+//                   Arg5.Name:           Tau0
+//                   Arg5.Title:          tau0
 //                   Arg5.Type:           Double_t
 //                   Arg5.EntryType:      Entry-C
 //                   Arg5.Default:        1.0
 //                   Arg5.AddLofValues:   No
 //                   Arg5.Base:           dec
 //                   Arg5.Orientation:    horizontal
-//                   Arg6.Name:           Tau1
-//                   Arg6.Title:          tau1
+//                   Arg6.Name:           A1
+//                   Arg6.Title:          a1
 //                   Arg6.Type:           Double_t
 //                   Arg6.EntryType:      Entry-C
 //                   Arg6.Default:        1.0
 //                   Arg6.AddLofValues:   No
 //                   Arg6.Base:           dec
 //                   Arg6.Orientation:    horizontal
+//                   Arg7.Name:           Tau1
+//                   Arg7.Title:          tau1
+//                   Arg7.Type:           Double_t
+//                   Arg7.EntryType:      Entry-C
+//                   Arg7.Default:        1.0
+//                   Arg7.AddLofValues:   No
+//                   Arg7.Base:           dec
+//                   Arg7.Orientation:    horizontal
 //-Exec
 //////////////////////////////////////////////////////////////////////////////
 
@@ -107,8 +116,10 @@ Double_t multi_exp(Double_t * x, Double_t * par) {
 //////////////////////////////////////////////////////////////////////////////
 
    Double_t fitval  = 0;
+   Double_t c0 = par[0];
+   fitval = c0;
    for (Int_t i = 0; i < fitNofTaus; i ++) {
-      fitval = fitval + par[2 * i] * exp(-x[0] * log(2) / par[2 * i + 1]);
+      fitval += par[2 * i + 1] * exp(-x[0] * log(2) / par[2 * i + 2]);
    }
    return fitBinWidth * fitval;
 }
@@ -116,6 +127,7 @@ Double_t multi_exp(Double_t * x, Double_t * par) {
 
 void taufit(const Char_t * File,
          Int_t NofTaus = 1,
+         Double_t C = 0.0, Bool_t FixC = kFALSE,
          Double_t A0 = 1.0, Bool_t FixA0 = kFALSE,
          Double_t Tau0 = 1.0, Bool_t FixTau0 = kFALSE,
          Double_t A1 = 1.0, Bool_t FixA1 = kFALSE,
@@ -147,21 +159,23 @@ void taufit(const Char_t * File,
 	g->Draw("a*");										// draw graph: asteriscs + error bars
 
 	// set up fit function
-	TF1 * f = new TF1("taufit", multi_exp, g->GetXaxis()->GetXmax(), g->GetXaxis()->GetXmin(), 2 * fitNofTaus);
+	TF1 * f = new TF1("taufit", multi_exp, g->GetXaxis()->GetXmax(), g->GetXaxis()->GetXmin(), 2 * fitNofTaus + 1);
 
-	f->SetParName(0, "a_{0}");				// set par name (in latex mode!)
-	f->SetParameter(0, A0); 				// start value
-	if (FixA0) f->FixParameter(0, A0);
-	f->SetParName(1, "#tau_{0}");			// ...
-	f->SetParameter(1, Tau0);
-	if (FixTau0) f->FixParameter(1, Tau0);
+	f->SetParName(0, "c");					// set par name (in latex mode!)
+	if (FixC) f->FixParameter(0, C);
+	f->SetParName(1, "a_{0}");				// set par name (in latex mode!)
+	f->SetParameter(1, A0); 				// start value
+	if (FixA0) f->FixParameter(1, A0);
+	f->SetParName(2, "#tau_{0}");			// ...
+	f->SetParameter(2, Tau0);
+	if (FixTau0) f->FixParameter(2, Tau0);
 	if (fitNofTaus == 2) {
-		f->SetParName(2, "a_{1}");
-		f->SetParameter(2, A1);
-		if (FixA1) f->FixParameter(2, A1);
-		f->SetParName(3, "#tau_{1}");
-		f->SetParameter(3, Tau1);
-		if (FixTau1) f->FixParameter(3, Tau1);
+		f->SetParName(3, "a_{1}");
+		f->SetParameter(3, A1);
+		if (FixA1) f->FixParameter(3, A1);
+		f->SetParName(4, "#tau_{1}");
+		f->SetParameter(4, Tau1);
+		if (FixTau1) f->FixParameter(4, Tau1);
 	}
 	f->SetLineColor(2);
 	g->Fit(f);								// do the fit
@@ -171,9 +185,9 @@ void taufit(const Char_t * File,
 	Double_t x0 = g->GetXaxis()->GetXmin()  + (g->GetXaxis()->GetXmax() - g->GetXaxis()->GetXmin()) * 0.6;
 	Double_t y0 = g->GetYaxis()->GetXmin()  + (g->GetYaxis()->GetXmax() - g->GetYaxis()->GetXmin()) * 0.6;
 	if (fitNofTaus > 1) {
-		tl->DrawLatex(x0, y0, Form("f(x) = #sum_{i=0}^{%d}a_{i} #upoint e^{ -x #upoint 6.9315 / #tau_{i}}", fitNofTaus - 1));
+		tl->DrawLatex(x0, y0, Form("f(x) = c + #sum_{i=0}^{%d}a_{i} #upoint e^{ -x #upoint 6.9315 / #tau_{i}}", fitNofTaus - 1));
 	} else {
-		tl->DrawLatex(x0, y0, "f(x) = a_{0} #upoint e^{ -x #upoint 6.9315 / #tau_{0}}");
+		tl->DrawLatex(x0, y0, "f(x) = c + a_{0} #upoint e^{ -x #upoint 6.9315 / #tau_{0}}");
 	}
 
 	fileName.ReplaceAll(".dat", ".ps"); 	// change file extension from .dat to .ps
