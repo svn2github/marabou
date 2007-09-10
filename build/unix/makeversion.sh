df76019c1ceb26fc005ccbb65c70a50eaeccf536
Rudolf.Lutter <Rudolf.Lutter@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2007-09-10 06:35:11 +0000

    Changed version.cxx to create file MVersion.h
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@1426 514c3d59-69d9-4452-b10b-b80f2e82d9e4

--- build/unix/makeversion.sh
+++ build/unix/makeversion.sh
@@ -4,6 +4,7 @@
 # Called by main Makefile as soon as build/version_number has been updated.
 #
 # Author: Fons Rademakers, 28/4/2000
+# Adopted for use with MARABOU: R. Lutter, 10-Sep-2007
 
 CINT=cint/main/cint_tmp
 SCRIPT=build/version.cxx
@@ -13,14 +14,10 @@ $CINT $SCRIPT
 echo "New version is `cat build/version_number`. Updating dependencies..."
 
 # compile all files that were out-of-date prior to makeversion.sh
-make -o base/inc/RVersion.h
+make -o base/inc/MVersion.h
 
 # touch all files that don't need recompilation (need to do this 3 times
 # to walk through chain of dependencies)
 make -s -t; make -s -t; make -s -t
-
-# recompile only base/src/TROOT.cxx
-touch base/src/TROOT.cxx
-make
 
 echo "root-config --version reports: `bin/root-config --prefix=. --version`"
