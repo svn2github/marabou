69f1a90ba45078394adc1ba6840feb0308ea7f4d
Rudolf.Lutter <Rudolf.Lutter@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2008-01-17 12:41:30 +0000

    *** empty log message ***
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@1508 514c3d59-69d9-4452-b10b-b80f2e82d9e4

---
+++ macros/LoadXMLLibs.C
@@ -0,0 +1,20 @@
+//________________________________________________________________[ROOT MACRO]
+//////////////////////////////////////////////////////////////////////////////
+// Name:             LoadXMLLibs.C
+// Purpose:          Load MARaBOU's XML libraries
+// Description:      Loads XML libraries via LD_LIBRARY_PATH
+// Author:           R. Lutter
+// Mailto:           <a href=mailto:rudi.lutter@physik.uni-muenchen.de>R. Lutter</a>
+// Revision:         $Id: LoadXMLLibs.C,v 1.1 2008-01-17 12:41:30 Rudolf.Lutter Exp $       
+// Date:           
+//////////////////////////////////////////////////////////////////////////////
+
+{
+	cout << "[Loading MARaBOU's XML libs]" << endl;
+	Int_t sts = gSystem->Load("libGed.so");
+	if (sts == -1) gSystem->Exit(1);
+	Int_t sts = gSystem->Load("libTMrbUtils.so");
+	if (sts == -1) gSystem->Exit(1);
+	TMrbSystem ux;
+	if (ux.Load("libXMLParser.so:libTMrbXML.so") == -1) gSystem->Exit(1);
+}
