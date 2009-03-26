b56fad35c4b74867e1b4645ce4caee52435e78d3
Rudolf.Lutter <Rudolf.Lutter@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2009-03-26 11:35:43 +0000

    *** empty log message ***
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@1749 514c3d59-69d9-4452-b10b-b80f2e82d9e4

--- snake-pgm/Module.mk
+++ snake-pgm/Module.mk
@@ -1,4 +1,3 @@
-<<<<<<< Module.mk
 # Module.mk for ddacontrol / ddaexec module
 # Author: Rudi Lutter, 23-Mar-2009
 
@@ -37,35 +36,6 @@ ALLEXECS		+= $(DDAXME)
 ALLLIBS 		+= $(DDALIB)
 
 ALLHDRS 		+= $(patsubst $(MODDIRI)/%.h,include/%.h,$(DDAH))
-=======
-# Module.mk for snake server module
-
-MODDIR       := snake-lib
-MODDIRS      := $(MODDIR)/src
-MODDIRI      := $(MODDIR)/inc
-
-DDADIR      := $(MODDIR)
-DDADIRS     := $(DDADIR)/src
-DDADIRI     := $(DDADIR)/inc
-
-DDAL        := $(MODDIRI)/LinkDef.h
-DDADS       := $(MODDIRS)/G__DDADict.cxx
-DDADO       := $(DDADS:.cxx=.o)         
-
-DDAO       := $(MODDIRS)/TSnkDDA0816.o  $(MODDIRS)/TSnkDDAChannel.o  $(DDADO)                
-
-DDAH        := $(filter-out $(MODDIRI)/LinkDef%,$(wildcard $(MODDIRI)/*.h))
-
-DDADEP      := $(DDAO:.o=.d)
-
-DDALIB      := $(LPATH)/libTSnkDDA0816.$(SOEXT)
-DDA0816DIRI	:= dda0816/include
-DDA0816H	:= $(wildcard $(DDA0816DIRI)/*.h)
-
-# used in the main Makefile
-ALLHDRS     += $(patsubst $(MODDIRI)/%.h,include/%.h,$(DDAH))
-ALLLIBS     += $(DDALIB)
->>>>>>> 1.8
 
 # include all dependency files
 INCLUDEFILES	+= $(DDADEP)
@@ -76,7 +46,6 @@ SNKLIBS			:= -L$(LPATH) -lTSnkDDA0816 -lTMrbUtils -lTGMrbUtils
 ##### local rules #####
 
 include/%.h:    $(DDADIRI)/%.h
-<<<<<<< Module.mk
 				cp $< $@
 
 $(DDACTRL): 	$(DDALO) $(DDACO)
@@ -87,57 +56,21 @@ $(DDAX):	 	$(DDAXO)
 
 $(DDAXME):	 	$(DDAXMEO)
 				$(LD) -g $(LDFLAGS) $(DDAXMEO) $(SNKLIBS) $(ROOTGLIBS) -o $(DDAXME)
-=======
-				cp $< $@
->>>>>>> 1.8
-
-<<<<<<< Module.mk
 $(DDALIB):		$(DDAGO) $(DDALO)
 				@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" \
 				"$(SOFLAGS)" libDDAControl.$(SOEXT) $@ "$(DDALO) $(DDAGO)"
-=======
-$(DDALIB):		$(DDAO) 
-				@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" "$(SOFLAGS)" libTSnkDDA0816.$(SOEXT) $@ "$(DDAO)" 
->>>>>>> 1.8
-
-<<<<<<< Module.mk
 $(DDAGS):		$(DDAH) $(DDAL)
 				$(ROOTCINT) -f $@ -c -p -Iinclude $(DDAH) $(DDAL)
-=======
-$(DDADS):		$(DDAH) $(DDAL)
-				@echo "Generating dictionary $@..."
-				$(ROOTCINT) -f $@ -c -Iinclude $(DDAH) $(DDAL)
->>>>>>> 1.8
-
-<<<<<<< Module.mk
 $(DDAGO):		$(DDAGS)
 				$(CXX) $(NOOPT) $(CXXFLAGS) -I. -o $@ -c $<
-=======
-$(DDADO):		$(DDADS)
-				$(CXX) $(NOOPT) $(CXXFLAGS) -DROOTVERSION=$(ROOTVERS) -I. -o $@ -c $<
->>>>>>> 1.8
-
-<<<<<<< Module.mk
 all-dda:		$(DDACTRL)
-=======
-all-dda:		$(DDALIB)
->>>>>>> 1.8
 
 clean-dda:
-<<<<<<< Module.mk
 				@rm -f $(DDAO) $(DDAMO) $(DDAGO) $(DDAG)
-=======
-				@rm -f $(DDAO) $(DDADO) $(DDADS)
->>>>>>> 1.8
 
 clean::         clean-dda
 
-<<<<<<< Module.mk
 distclean-dda:	clean-dda
 				@rm -f $(DDADEP) $(DDAG) $(DDACTRL) $(DDALIB)
-=======
-distclean-dda:	clean-dda
-				@rm -f $(DDADEP) $(DDADS) $(DDADH) $(DDALIB)
->>>>>>> 1.8
 
 distclean::     distclean-dda
