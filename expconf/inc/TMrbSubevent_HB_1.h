e581ed466bff26cd9a491c36b9be5ce66a4a5618
Rudolf.Lutter <Rudolf.Lutter@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2009-06-24 14:07:34 +0000

    *** empty log message ***
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@1832 514c3d59-69d9-4452-b10b-b80f2e82d9e4

--- expconf/inc/TMrbSubevent_HB_1.h
+++ expconf/inc/TMrbSubevent_HB_1.h
@@ -9,7 +9,7 @@
 //                                         format similar to [10,1] but with hitbuffer
 // Description:    Class definitions to implement a configuration front-end for MARaBOU
 // Author:         R. Lutter
-// Revision:       $Id: TMrbSubevent_HB_1.h,v 1.3 2009-06-24 13:59:12 Rudolf.Lutter Exp $       
+// Revision:       $Id: TMrbSubevent_HB_1.h,v 1.4 2009-06-24 14:07:34 Rudolf.Lutter Exp $       
 // Date:           
 // Keywords:
 //////////////////////////////////////////////////////////////////////////////
@@ -44,7 +44,7 @@ class TMrbSubevent_HB_1 : public TMrbSubevent {
 	public:
 
 		TMrbSubevent_HB_1() {}; 				// default ctor
-		TMrbSubevent_HB_1(const Char_t * SevtName, const Char_t * SevtTitle = "");	// create a new hit buffer subevent
+		TMrbSubevent_HB_1(const Char_t * SevtName, const Char_t * SevtTitle = "", Int_t Crate = -1);	// create a new hit buffer subevent
 		~TMrbSubevent_HB_1() {};	 			// remove current subevent from list
 
 		Bool_t MakeReadoutCode(ofstream & RdoStrm, TMrbConfig::EMrbReadoutTag TagIndex, 		// generate part of code for this subevent
