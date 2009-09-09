bc8d0c3f317f0abb58bf56799f74e0ed8dc5b20e
Rudolf.Lutter <Rudolf.Lutter@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2009-09-09 14:36:03 +0000

    VulomTB now implemented in Config
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@1885 514c3d59-69d9-4452-b10b-b80f2e82d9e4

--- expconf/inc/TMrbVulomTB.h
+++ expconf/inc/TMrbVulomTB.h
@@ -8,7 +8,7 @@
 // Class:          TMrbVulomTB        -- Vulom TriggerBox
 // Description:    Class definitions to implement a configuration front-end for MARaBOU
 // Author:         R. Lutter
-// Revision:       $Id: TMrbVulomTB.h,v 1.1 2009-08-06 08:30:58 Rudolf.Lutter Exp $
+// Revision:       $Id: TMrbVulomTB.h,v 1.2 2009-09-09 14:36:03 Rudolf.Lutter Exp $
 // Date:
 // Keywords:
 //////////////////////////////////////////////////////////////////////////////
@@ -27,9 +27,6 @@ namespace std {} using namespace std;
 
 #include "TMrbVMEModule.h"
 
-class TMrbVMEChannel;
-class TMrbSubevent;
-
 //______________________________________________________[C++ CLASS DEFINITION]
 //////////////////////////////////////////////////////////////////////////////
 // Name:           TMrbVulomTB
@@ -43,17 +40,43 @@ class TMrbVulomTB : public TMrbVMEModule {
 	public:
 		enum				{	kSegSize			=	0x10000	};
 		enum				{	kAddrMod			=	0x09	};
+		enum				{	kNofChannels		=	16		};
+		enum				{	kMaxScaleDown		=	15		};
 
+		enum				{	kScSetInput			=	BIT(0)		};
+		enum				{	kScSetInhibit		=	BIT(1)		};
+		enum				{	kScSetScaleDown		=	BIT(2)		};
+		enum				{	kScSetAll			=	kScSetInput | kScSetInhibit | kScSetScaleDown	};
 
 	public:
 
 		TMrbVulomTB() {};  												// default ctor
-		TMrbVulomTB(const Char_t * ModuleName, UInt_t BaseAddr); 			// define a new adc
+		TMrbVulomTB(const Char_t * ModuleName, UInt_t BaseAddr, Int_t NofScalerChans = kNofChannels, UInt_t ScalerSet = kScSetAll); 			// define a new trigbox
 		~TMrbVulomTB() {};												// default dtor
+
+		inline Int_t GetNofScalerChans() { return(fNofScalerChans); };
 
 		virtual inline const Char_t * GetMnemonic() const { return("vulomTB"); }; 	// module mnemonic
 
-		inline void Help() { gSystem->Exec(Form("mrbHelp %s", this->ClassName())); };
+		Bool_t MakeReadoutCode(ofstream & RdoStrm, TMrbConfig::EMrbModuleTag TagIndex);  	// generate part of code
+		Bool_t MakeReadoutCode(ofstream & RdoStrm, TMrbConfig::EMrbModuleTag TagIndex, TMrbVMEChannel * Channel, Int_t Value = 0);  	// generate code for given channel
+
+		inline void SetEnableMask(UInt_t EnableMask) { fEnableMask = EnableMask; };
+		inline UInt_t GetEnableMask() { return(fEnableMask); };
+		Bool_t SetScaleDown(Int_t ScaleDown, Int_t ChannelNo);
+		Int_t GetScaleDown(Int_t ChannelNo);
+		inline void SetRdoScaleDown(Int_t RdoScd) { fRdoScaleDown = RdoScd; };
+		inline Int_t GetRdoScaleDown() { return(fRdoScaleDown); };
+
+inline void Help() { gSystem->Exec(Form("mrbHelp %s", this->ClassName())); };
+
+	protected:
+		Int_t fNofScalerChans;
+		Int_t fNofScalerSets;
+		UInt_t fScalerSetMask;
+		UInt_t fEnableMask;
+		TArrayI fScaleDown;
+		Int_t fRdoScaleDown;
 
 	ClassDef(TMrbVulomTB, 1)		// [Config] VulomTB, triggerBox GSI
 };
--- expconf/src/TMrbVulomTB.cxx
+++ expconf/src/TMrbVulomTB.cxx
@@ -6,7 +6,7 @@
 // Keywords:
 // Author:         R. Lutter
 // Mailto:         <a href=mailto:rudi.lutter@physik.uni-muenchen.de>R. Lutter</a>
-// Revision:       $Id: TMrbVulomTB.cxx,v 1.3 2009-08-06 09:52:22 Rudolf.Lutter Exp $
+// Revision:       $Id: TMrbVulomTB.cxx,v 1.4 2009-09-09 14:36:03 Rudolf.Lutter Exp $
 // Date:
 //////////////////////////////////////////////////////////////////////////////
 
@@ -34,20 +34,31 @@ extern TMrbLogger * gMrbLog;
 ClassImp(TMrbVulomTB)
 
 
-TMrbVulomTB::TMrbVulomTB(const Char_t * ModuleName, UInt_t BaseAddr) :
+TMrbVulomTB::TMrbVulomTB(const Char_t * ModuleName, UInt_t BaseAddr, Int_t NofScalerChans, UInt_t ScalerSets) :
 									TMrbVMEModule(ModuleName, "VulomTB", BaseAddr,
 																TMrbVulomTB::kAddrMod,
 																TMrbVulomTB::kSegSize,
-																0, 16, 1 << 31) {
+																0, 3 * NofScalerChans, 1 << 31) {
 //__________________________________________________________________[C++ CTOR]
 //////////////////////////////////////////////////////////////////////////////
 // Name:           TMrbVulomTB
-// Purpose:        Create an adc of type Caen V785
+// Purpose:        Create a vulom module
 // Arguments:      Char_t * ModuleName      -- name of camac module
 //                 UInt_t BaseAddr          -- base addr
+//                 Int_t NofScalerChans     -- number of scaler channels
+//                 UInt_t ScalerSets        -- scaler sets
 // Results:        --
 // Exceptions:
-// Description:    Creates a vme adc of type CAEN V785.
+// Description:    Creates a vulom of type trigger box.
+//                 ScalerSets may be used to (de)activate scalers sets:
+//                       kScSetInput, BIT(0)       - input
+//                       kScSetInhibit, BIT(1)     - after inhibit
+//                       kScSetScaleDown, BIT(2)   - after scale down
+//                       and any combinations
+//                       default is kScSetAll, 0x7 - all 3 sets of scalers
+//                 NofScalerChans denotes the number of scaler channels used.
+//                 The number of channels as seen by MARaBOU is then
+//                       fNofChannels =  (# scaler sets) * NofScalerChans
 // Keywords:
 //////////////////////////////////////////////////////////////////////////////
 
@@ -77,6 +88,18 @@ TMrbVulomTB::TMrbVulomTB(const Char_t * ModuleName, UInt_t BaseAddr) :
 			fNofShortsPerChannel = 2;
 			fNofShortsPerDatum = 2;
 			fNofDataBits = 32;
+			fBlockReadout = kTRUE;
+
+			fNofScalerSets = 0;
+			fScalerSetMask = ScalerSets;
+			if (ScalerSets & kScSetInput) fNofScalerSets++;
+			if (ScalerSets & kScSetInhibit) fNofScalerSets++;
+			if (ScalerSets & kScSetScaleDown) fNofScalerSets++;
+			fNofScalerChans = NofScalerChans;
+			fScaleDown.Set(NofScalerChans);
+			fEnableMask = 0;
+			for (Int_t i = 0; i < NofScalerChans; i++) fEnableMask |= (1 << i);
+			fRdoScaleDown = 1;
 
 			codeFile = fModuleID.GetName();
 			codeFile += ".code";
@@ -89,4 +112,202 @@ TMrbVulomTB::TMrbVulomTB(const Char_t * ModuleName, UInt_t BaseAddr) :
 			this->AllocateHistograms(kFALSE);				// don't allocate histograms
 		}
 	}
+}
+
+Bool_t TMrbVulomTB::SetScaleDown(Int_t ScaleDown, Int_t ChannelNo) {
+//________________________________________________________________[C++ METHOD]
+//////////////////////////////////////////////////////////////////////////////
+// Name:           TMrbVulomTB::SetScaleDown
+// Purpose:        Set scale down value
+// Arguments:      Int_t ScaleDown      -- scale down value
+//                 Int_t ChannelNo      -- channel number
+// Results:        kTRUE/kFALSE
+// Description:    Defines scale down value for a given channel
+//////////////////////////////////////////////////////////////////////////////
+
+	if (ChannelNo < 0 || ChannelNo > kNofChannels) {
+		gMrbLog->Err() << "[" << this->GetName() << "]: Channel out of range - " << ChannelNo << " (should be in [0," << kNofChannels << "])"<< endl;
+		gMrbLog->Flush(this->ClassName(), "SetScaleDown");
+		return(kFALSE);
+	}
+	if (ScaleDown < 1 || ScaleDown > kMaxScaleDown) {
+		gMrbLog->Err() << "[" << this->GetName() << ", chn " << ChannelNo << "]: Scale down out of range - " << ScaleDown << " (should be in [1," << kMaxScaleDown << "])"<< endl;
+		gMrbLog->Flush(this->ClassName(), "SetScaleDown");
+		return(kFALSE);
+	}
+	fScaleDown[ChannelNo] = ScaleDown;
+	return(kTRUE);
+}
+
+Int_t TMrbVulomTB::GetScaleDown(Int_t ChannelNo) {
+//________________________________________________________________[C++ METHOD]
+//////////////////////////////////////////////////////////////////////////////
+// Name:           TMrbVulomTB::GetScaleDown
+// Purpose:        Get scale down value
+// Arguments:      Int_t ChannelNo      -- channel number
+// Results:        Int_t ScaleDown      -- scale down value
+// Description:    Returns scale down value for a given channel
+//////////////////////////////////////////////////////////////////////////////
+
+	if (ChannelNo < 0 || ChannelNo > kNofChannels) {
+		gMrbLog->Err() << "[" << this->GetName() << "]: Channel out of range - " << ChannelNo << " (should be in [0," << kNofChannels << "])"<< endl;
+		gMrbLog->Flush(this->ClassName(), "GetScaleDown");
+		return(-1);
+	}
+	return(fScaleDown[ChannelNo]);
+}
+
+Bool_t TMrbVulomTB::MakeReadoutCode(ofstream & RdoStrm, TMrbConfig::EMrbModuleTag TagIndex) {
+//________________________________________________________________[C++ METHOD]
+//////////////////////////////////////////////////////////////////////////////
+// Name:           TMrbVulomTB::MakeReadoutCode
+// Purpose:        Write a piece of code for a vulom triggerbox
+// Arguments:      ofstream & RdoStrm         -- file output stream
+//                 EMrbModuleTag TagIndex     -- index of tag word taken from template file
+// Results:        kTRUE/kFALSE
+// Exceptions:
+// Description:    Writes code for readout of a vulom triggerbox.
+// Keywords:
+//////////////////////////////////////////////////////////////////////////////
+
+	TString mnemoLC, mnemoUC;
+
+	if (!fCodeTemplates.FindCode(TagIndex)) {
+		gMrbLog->Err()	<< "No code loaded for tag "
+						<< gMrbConfig->fLofModuleTags.FindByIndex(TagIndex)->GetName() << endl;
+		gMrbLog->Flush(this->ClassName(), "MakeReadoutCode");
+		return(kFALSE);
+	}
+
+	mnemoLC = this->GetMnemonic();
+	mnemoUC = mnemoLC;
+	mnemoUC.ToUpper();
+
+	switch (TagIndex) {
+		case TMrbConfig::kModuleDefs:
+		case TMrbConfig::kModuleInitCommonCode:
+		case TMrbConfig::kModuleInitModule:
+			{
+			  fCodeTemplates.InitializeCode();
+			  fCodeTemplates.Substitute("$moduleName", this->GetName());
+			  fCodeTemplates.Substitute("$moduleTitle", this->GetTitle());
+			  fCodeTemplates.Substitute("$modulePosition", this->GetPosition());
+			  fCodeTemplates.Substitute("$mnemoLC", mnemoLC);
+			  fCodeTemplates.Substitute("$mnemoUC", mnemoUC);
+			  fCodeTemplates.Substitute("$baseAddr", (Int_t) this->GetBaseAddr(), 16);
+			  fCodeTemplates.Substitute("$channelMask", (Int_t) this->GetEnableMask(), 16);
+			  UInt_t scd = 0;
+			  Int_t shift = 0;
+			  for (Int_t i = 0; i < 8; i++, shift += 4) {
+				  if (i >= this->GetNofScalerChans()) break;
+				  scd |= (fScaleDown[i] << shift);
+			  }
+			  fCodeTemplates.Substitute("$scdLower", (Int_t) scd, 16);
+			  scd = 0;
+			  shift = 0;
+			  for (Int_t i = 9; i < 16; i++, shift += 4) {
+				  if (i >= this->GetNofScalerChans()) break;
+				  scd |= (fScaleDown[i] << shift);
+			  }
+			  fCodeTemplates.Substitute("$scdUpper", (Int_t) scd, 16);
+			  fCodeTemplates.WriteCode(RdoStrm);
+			}
+			break;
+		case TMrbConfig::kModuleClearModule:
+			fCodeTemplates.InitializeCode();
+			fCodeTemplates.Substitute("$moduleName", this->GetName());
+			fCodeTemplates.WriteCode(RdoStrm);
+			break;
+		case TMrbConfig::kModuleReadModule:
+			fCodeTemplates.InitializeCode("%B%");
+			fCodeTemplates.WriteCode(RdoStrm);
+			if (fScalerSetMask & kScSetInput) {
+				fCodeTemplates.InitializeCode("%RDO%");
+				fCodeTemplates.Substitute("$moduleName", this->GetName());
+				fCodeTemplates.Substitute("$scalerOffset", "VULOM_TBOX_SCALER1");
+				fCodeTemplates.Substitute("$nofChannels", this->GetNofScalerChans());
+				fCodeTemplates.WriteCode(RdoStrm);
+			}
+			if (fScalerSetMask & kScSetInhibit) {
+				fCodeTemplates.InitializeCode("%RDO%");
+				fCodeTemplates.Substitute("$moduleName", this->GetName());
+				fCodeTemplates.Substitute("$scalerOffset", "VULOM_TBOX_SCALER2");
+				fCodeTemplates.Substitute("$nofChannels", this->GetNofScalerChans());
+				fCodeTemplates.WriteCode(RdoStrm);
+			}
+			if (fScalerSetMask & kScSetScaleDown) {
+				fCodeTemplates.InitializeCode("%RDO%");
+				fCodeTemplates.Substitute("$moduleName", this->GetName());
+				fCodeTemplates.Substitute("$scalerOffset", "VULOM_TBOX_SCALER3");
+				fCodeTemplates.Substitute("$nofChannels", this->GetNofScalerChans());
+				fCodeTemplates.WriteCode(RdoStrm);
+			}
+			fCodeTemplates.InitializeCode("%E%");
+			fCodeTemplates.Substitute("$rdoScaleDown", this->GetRdoScaleDown());
+			fCodeTemplates.WriteCode(RdoStrm);
+			break;
+		case TMrbConfig::kModuleStartAcquisition:
+			fCodeTemplates.InitializeCode();
+			fCodeTemplates.Substitute("$rdoScaleDown", this->GetRdoScaleDown());
+			fCodeTemplates.WriteCode(RdoStrm);
+			break;
+		case TMrbConfig::kModuleFinishReadout:
+		case TMrbConfig::kModuleStopAcquisition:
+		case TMrbConfig::kModuleStartAcquisitionGroup:
+		case TMrbConfig::kModuleStopAcquisitionGroup:
+			break;
+		case TMrbConfig::kModuleDefineLocalVarsReadout:
+		case TMrbConfig::kModuleUtilities:
+		case TMrbConfig::kModuleDefineLocalVarsInit:
+		case TMrbConfig::kModuleDefinePrototypes:
+		case TMrbConfig::kModuleDefineGlobals:
+			fCodeTemplates.InitializeCode();
+			fCodeTemplates.WriteCode(RdoStrm);
+			break;
+	}
+	return(kTRUE);
+}
+
+
+Bool_t TMrbVulomTB::MakeReadoutCode(ofstream & RdoStrm,	TMrbConfig::EMrbModuleTag TagIndex,
+															TMrbVMEChannel * Channel,
+															Int_t Value) {
+//________________________________________________________________[C++ METHOD]
+//////////////////////////////////////////////////////////////////////////////
+// Name:           TMrbVulomTB::MakeReadoutCode
+// Purpose:        Write a piece of code for a vulom triggerbox
+// Arguments:      ofstream & RdoStrm           -- file output stream
+//                 EMrbModuleTag TagIndex       -- index of tag word taken from template file
+//                 TMrbVMEChannel * Channel     -- channel
+//                 Int_t Value                  -- value to be set
+// Results:        kTRUE/kFALSE
+// Exceptions:
+// Description:    Writes code for readout of a vulom triggerbox.
+// Keywords:
+//////////////////////////////////////////////////////////////////////////////
+
+	TString mnemoLC, mnemoUC;
+
+	if (!fCodeTemplates.FindCode(TagIndex)) {
+		gMrbLog->Err()	<< "No code loaded for tag "
+						<< gMrbConfig->fLofModuleTags.FindByIndex(TagIndex)->GetName() << endl;
+		gMrbLog->Flush(this->ClassName(), "MakeReadoutCode");
+		return(kFALSE);
+	}
+
+	mnemoLC = this->GetMnemonic();
+	mnemoUC = mnemoLC;
+	mnemoUC.ToUpper();
+
+	switch (TagIndex) {
+		case TMrbConfig::kModuleInitChannel:
+		case TMrbConfig::kModuleWriteSubaddr:
+		case TMrbConfig::kModuleSetupReadout:
+		case TMrbConfig::kModuleReadChannel:
+		case TMrbConfig::kModuleIncrementChannel:
+		case TMrbConfig::kModuleSkipChannels:
+		case TMrbConfig::kModuleStoreData:
+			break;
+	}
+	return(kTRUE);
 }
