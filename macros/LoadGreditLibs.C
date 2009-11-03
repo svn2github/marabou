90b45a933359c49c10e19f1c0d8182460349b7e5
Otto.Schaile <Otto.Schaile@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2009-11-03 08:26:13 +0000

    new LoadGreditLibs.C
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@1915 514c3d59-69d9-4452-b10b-b80f2e82d9e4

---
+++ macros/LoadGreditLibs.C
@@ -0,0 +1,27 @@
+//________________________________________________________________[ROOT MACRO]
+//////////////////////////////////////////////////////////////////////////////
+// Name:             LoadHprLibs.C
+// Purpose:          Load MARaBOU's utility libraries + HistPresent
+// Description:      Loads utility libraries via LD_LIBRARY_PATH
+// Author:           R. Lutter
+// Mailto:           <a href=mailto:rudi.lutter@physik.uni-muenchen.de>R. Lutter</a>
+// Revision:         $Id: LoadGreditLibs.C,v 1.1 2009-11-03 08:26:13 Otto.Schaile Exp $       
+// Date:           
+//////////////////////////////////////////////////////////////////////////////
+
+{
+	cout << "[Loading MARaBOU's utility libs]" << endl;
+	Int_t sts = gSystem->Load("libGed.so");
+	if (sts == -1) gSystem->Exit(1);
+	Int_t sts = gSystem->Load("libTMrbUtils.so");
+	if (sts == -1) gSystem->Exit(1);
+	TMrbSystem ux;
+	ux.PrintLoadPath(sts, "libTMrbUtils.so");
+	if (ux.Load("libTGMrbUtils.so") == -1) gSystem->Exit(1);
+//	ux.PrintLoadPath(sts, "libFitCal.so");
+//	if (ux.Load("libFitCal.so") == -1) gSystem->Exit(1);
+//	ux.PrintLoadPath(sts, "libHpr.so");
+//	if (ux.Load("libHpr.so") == -1) gSystem->Exit(1);
+	ux.PrintLoadPath(sts, "libGrEdit.so");
+	if (ux.Load("libGrEdit.so") == -1) gSystem->Exit(1);
+}
