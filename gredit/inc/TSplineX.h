d11613737972bbb314644116a0e037f2c8bfdb03
Otto.Schaile <Otto.Schaile@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2009-11-25 16:05:44 +0000

    fix int DistanceToPrimitive
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@1920 514c3d59-69d9-4452-b10b-b80f2e82d9e4

--- gredit/inc/TSplineX.h
+++ gredit/inc/TSplineX.h
@@ -78,7 +78,7 @@ protected:
 public:
    ControlGraph (Int_t npoints = 0, Double_t*  x = NULL, Double_t* y = NULL);
    virtual void  Delete(Option_t *option="") { std::cout << " no no " << std::endl; };
-	DistancetoPrimitive(Int_t px, Int_t py);
+	int DistancetoPrimitive(Int_t px, Int_t py);
    void ExecuteEvent(Int_t event, Int_t px, Int_t py);
    void SetParent(TSplineX* parent);
    TSplineX  *GetParent(){return fParent;};
