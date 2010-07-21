c158f4f023f346475b81cdeb5fddeedfe4807d19
Rudolf.Lutter <Rudolf.Lutter@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2010-07-21 11:08:31 +0000

    *** empty log message ***
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@1989 514c3d59-69d9-4452-b10b-b80f2e82d9e4

--- c_analyze/inc/C_analyze.h
+++ c_analyze/inc/C_analyze.h
@@ -2,37 +2,37 @@
 #define HPR_CANALYZE
 #include "TMbsControl.h"
 #include "TMbsSetup.h"
-#include "TMrbHelpBrowser.h" 
+#include "TMrbHelpBrowser.h"
 
 namespace std {} using namespace std;
 
 enum CAnalyzeCommandIdentifiers {
    M_CLEAR, M_START_STOP, M_PAUSE, M_CONFIG, M_RESET, M_SAVEMAP, M_DELETEMAP,
    M_INTERVAL_COMBO, M_DOWNSCALE, M_HELP,M_LOADPAR, C_TRIGGER,C_MASTER,C_READOUT,
-   C_MBSVERSION, R_FAKE, R_FILE, R_FILELIST, R_NET, R_ACTIVE, R_AUTO, R_PASSIVE, 
+   C_MBSVERSION, R_FAKE, R_FILE, R_FILELIST, R_NET, R_ACTIVE, R_AUTO, R_PASSIVE,
    M_MBSSTATUS, M_BUFSIZE, M_BUFSTREAM, M_NOEVENT,
    M_HELPC, M_HELPM, M_HELPBR, M_HELPNAME,
    M_COMMENT, M_RESETALL, M_RESETSEL, M_STARTEVENT, M_STOPEVENT, M_FROMTIME
    , M_TOTIME, M_GATEL, M_MBSSETUP, M_CODENAME, M_COMPILE
-   , M_MAXFILESIZE, M_MAXRUNTIME, M_MBSLOGLEVEL, M_SELECTTIME, M_SELECTNUMBER 
+   , M_MAXFILESIZE, M_MAXRUNTIME, M_MBSLOGLEVEL, M_SELECTTIME, M_SELECTNUMBER
    ,M_RUNNR, M_INPUT, M_DIR, M_MAPF, M_MAPS, M_HISTF, M_ROOTF, M_PARF
    ,M_QUIT, M_AVERAGE, M_SAVESETUP, M_AUTOSETUP, M_WHICHHIST, M_RESETLIST
    ,M_PAUSE_RESET, M_WARNHWM, M_HARDHWM, M_VERBLEV, M_CHKQUOTA, M_RELOAD
    ,M_DEBUG,M_MADEBUG, M_AUTORESTART, M_AUTORESTART_RUNTIME, M_HSAVEINTERVALL
    ,M_PLAYSOUND, M_SOUNDFILE, M_SOUNDPLAYER};
-enum M_status {M_ABSENT, M_STARTING, M_RUNNING, 
-               M_PAUSING, M_STOPPING, M_STOPPED, M_DIRTY, M_DIED, 
+enum M_status {M_ABSENT, M_STARTING, M_RUNNING,
+               M_PAUSING, M_STOPPING, M_STOPPED, M_DIRTY, M_DIED,
                M_CONFIGURED, M_CONFIGURING};
 const Int_t MAXTIMEOUT = 50;
 
-static const Int_t NPPC = 24, MBSV = 2, TRIGS = 2;
+static const Int_t NPPC = 25, MBSV = 2, TRIGS = 2;
 
-static const Char_t * masters[] = {"ppc-0", "ppc-1", "ppc-2", "ppc-3", "ppc-4", "ppc-5", "ppc-6", "ppc-7", "ppc-8", "ppc-1", "ppc-3", "ppc-5", "ppc-7", "ppc-10", "ppc-11", "ppc-12", "ppc-13", "ppc-14", "ppc-15", "ppc-16", "ppc-17", "ppc-18", "ppc-19", "ppc-20"};   
-static const Char_t * slaves[]  = {"ppc-0", "ppc-0", "ppc-2", "ppc-2", "ppc-4", "ppc-4", "ppc-6", "ppc-6", "ppc-8", "ppc-1", "ppc-3", "ppc-5", "ppc-7", "ppc-10", "ppc-11", "ppc-12", "ppc-13", "ppc-14", "ppc-15", "ppc-16", "ppc-17", "ppc-18", "ppc-19", "ppc-20"};
-static const Char_t * triggers[]  = {"VME", "CAMAC"}; 
+static const Char_t * masters[] = {"ppc-0", "ppc-1", "ppc-2", "ppc-3", "ppc-4", "ppc-5", "ppc-6", "ppc-7", "ppc-8", "ppc-1", "ppc-3", "ppc-5", "ppc-7", "ppc-10", "ppc-11", "ppc-12", "ppc-13", "ppc-14", "ppc-15", "ppc-16", "ppc-17", "ppc-18", "ppc-19", "ppc-20", "ppc-30"};
+static const Char_t * slaves[]  = {"ppc-0", "ppc-0", "ppc-2", "ppc-2", "ppc-4", "ppc-4", "ppc-6", "ppc-6", "ppc-8", "ppc-1", "ppc-3", "ppc-5", "ppc-7", "ppc-10", "ppc-11", "ppc-12", "ppc-13", "ppc-14", "ppc-15", "ppc-16", "ppc-17", "ppc-18", "ppc-19", "ppc-20", "ppc-30"};
+static const Char_t * triggers[]  = {"VME", "CAMAC"};
 
 static const Int_t  MINSOCKET = 9090, MAXSOCKET = 9095;
- 
+
 //______________________________________________________________________
 
 class FhMainFrame : public TGMainFrame {
@@ -43,7 +43,7 @@ private:
 
    TGCompositeFrame   *fHFr, *fHFrHalf,*fLabelFr;
    TRootEmbeddedCanvas *fRateHist;
-   TGTextButton        *fConfigButton, *fStartStopButton, *fClearButton, 
+   TGTextButton        *fConfigButton, *fStartStopButton, *fClearButton,
                       *fPauseButton, *fResetButton, *fQuitButton,
                       *fSaveMapButton,
                       *fParButton , *fMbsSetupButton, *fWhichHistButton;
@@ -52,17 +52,17 @@ private:
 
    TGTextEntry        *fTeMap, *fTeMapSize, *fTeFile,
                       *fTeRootFile, *fTeHistFile,*fTeRunNr,
-                      *fTeComment, *fTeParFile, 
+                      *fTeComment, *fTeParFile,
                       *fTeDir;
    TGTextBuffer       *fTbMap, *fTbMapSize, *fTbFile,
-                      *fTbRootFile, *fTbHistFile, *fTbRunNr, 
-                      *fTbComment, *fTbParFile, 
+                      *fTbRootFile, *fTbHistFile, *fTbRunNr,
+                      *fTbComment, *fTbParFile,
                       *fTbFileList,
                       *fTbDir,*fTbConvTime;
    TGLayoutHints      *fLO1, *fLO2, *fLO3, *fLO4, *fLO5, *fLR1;
 
    TGComboBox         *fCbTrigger, *fCbMaster, *fCbReadout, *fCbMbsVersion;
-   
+
    TGRadioButton      *fRFake, *fRFile, *fRFileList, *fRNet, *fRActive,*fRAuto, *fRPassive;
 
    TGMenuBar          *fMenuBar;
@@ -71,8 +71,8 @@ private:
    TGLayoutHints      *fMenuBarLayout, *fMenuBarItemLayout, *fMenuBarHelpLayout;
 
    TString *fInputFile, *fOutputFile, *fMap,
-           *fHistList, *fRootFile, *fHistFile, *fDefFile, *fInputSource, 
-           *fRunNr, *fOldRunNr, *fComment, *fPar, 
+           *fHistList, *fRootFile, *fHistFile, *fDefFile, *fInputSource,
+           *fRunNr, *fOldRunNr, *fComment, *fPar,
            *fMbsVersion, *fDir, *fMaster, *fReadout,
            *fTrigger, *fCodeName, *fFromTime, *fToTime,
            *fResetList, *fOurPidFile, *fHelpFile,
@@ -86,7 +86,7 @@ private:
    Int_t   fC_Status;
    Int_t   fWasStarted;
    Int_t   fEvents_before;
-   Int_t   fBufSize, fBuffers, fGateLength, fDownscale, 
+   Int_t   fBufSize, fBuffers, fGateLength, fDownscale,
            fStartEvent, fStopEvent, fMapSize, fMaxFileSize, fMaxRunTime,
            fVerbLevel, fHardHWM, fWarnHWM, fAutoRestart, fAutoRestartRT,
 			  fWaitedForStop, fHsaveIntervall, fPlaySound;
@@ -99,8 +99,8 @@ private:
    Int_t   fDebugMode;
    Int_t   fMessageIntervall;
 
-   Int_t   fSockNr; 
-   TSocket  * fComSocket; 
+   Int_t   fSockNr;
+   TSocket  * fComSocket;
    Int_t  fAttachid;
    Int_t  fAttachsock;
    MessageServer * fMessageServer;
@@ -120,9 +120,9 @@ private:
 
    GContext_t * fPYellowTextGC;
 
-   
+
 public:
-   FhMainFrame(const TGWindow *p, UInt_t w, UInt_t h, 
+   FhMainFrame(const TGWindow *p, UInt_t w, UInt_t h,
                Int_t attachid = 0, Int_t attachsock = 0);
    virtual ~FhMainFrame();
 
@@ -150,7 +150,7 @@ public:
    void   SetRadioButtons(Bool_t);
    Int_t IsAnalyzeRunning(Int_t);
    Int_t MessageToM_analyze(const char *);
-   TGTextEntry * GetDirText(){return fTeDir;}; 
+   TGTextEntry * GetDirText(){return fTeDir;};
 };
 #endif
 
