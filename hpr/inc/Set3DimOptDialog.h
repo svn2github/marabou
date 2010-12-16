37a50cbf2bb44cd6bf720fd0334a76d594557cc6
Otto.Schaile <Otto.Schaile@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2010-12-16 14:51:35 +0000

    3dimDia
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@2052 514c3d59-69d9-4452-b10b-b80f2e82d9e4

---
+++ hpr/inc/Set3DimOptDialog.h
@@ -0,0 +1,56 @@
+#ifndef SET3DIMOPTHISTDIALOG
+#define SET3DIMOPTHISTDIALOG
+#include "TGWindow.h"
+#include "TRootCanvas.h"
+#include "TGMrbValuesAndText.h"
+#include <iostream>
+//_____________________________________________________________________________________
+
+class Set3DimOptDialog : public TObject {
+
+private:
+   void *fValp[100];
+   TList *fRow_lab;
+   TGMrbValuesAndText *fDialog;
+   TCanvas      *fCanvas;
+	TH1          *fHist;
+   TString fDrawOpt;
+   Int_t fOptRadio[4];
+   const char *fDrawOpt3DimArray[4];
+   TString fDrawOpt3Dim;
+   Color_t fHistFillColor3Dim;
+   Color_t fHistLineColor3Dim;
+   Color_t fMarkerColor3Dim; 
+	Color_t f3DimBackgroundColor;
+   Style_t fMarkerStyle3Dim;  
+   Size_t  fMarkerSize3Dim; 
+	Int_t   fBidSCAT;
+	Int_t   fBidBOX;
+	Int_t   fBidBOX1;
+	Int_t   fBidARR;
+	Int_t   fBidTEXT;
+	Int_t   fBidMarkerColor;
+	Int_t   fBidMarkerStyle;
+	Int_t   fBidMarkerSize;
+	Int_t   fBidLineColor;
+	Int_t   fBidFillColor;
+	Int_t   fUseGL;
+	
+public:
+
+   Set3DimOptDialog(TGWindow * win = NULL);
+   ~Set3DimOptDialog() {};
+   void RecursiveRemove(TObject * obj);
+   void CloseDialog();
+   void SetHistAttNow(TCanvas *canvas);
+   void SetHistAtt(TCanvas *canvas);
+   void SetHistAttPermLocal();
+   void SetHistAttPerm();
+   void SaveDefaults();
+   void RestoreDefaults();
+   void CloseDown(Int_t wid);
+   void CRButtonPressed(Int_t wid, Int_t bid, TObject *obj);
+
+ClassDef(Set3DimOptDialog,0)
+};
+#endif
