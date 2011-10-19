9a36fd84dda35f0defb3d332ccfc266c31469cc2
Marabou <Marabou@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2011-10-19 12:30:28 +0000

    *** empty log message ***
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@2157 514c3d59-69d9-4452-b10b-b80f2e82d9e4

--- macros/mbssetup.C
+++ macros/mbssetup.C
@@ -1,11 +1,12 @@
 {
 	static const char setred[]      =   "\033[31m";
+	static const char setblack[]    =   "\033[39m";
 	gROOT->Macro("LoadUtilityLibs.C");
 	gROOT->Macro("LoadConfigLibs.C");
 	setup = new TMbsSetup(".mbssetup");
 	TString evtBuilder = gEnv->GetValue("TMbsSetup.EventBuilder.Name", "");
 	if (evtBuilder.IsNull()) {
-		cerr << setred << "mbssetup.C: Name of eventBuilder missing: Please define \"TMbsSetup.EventBuilder.Name\" in .rootrc" << endl;
+		cerr << setred << "mbssetup.C: Name of eventBuilder missing: Please define \"TMbsSetup.EventBuilder.Name\" in .rootrc" << setblack << endl;
 		gSystem->Exit(0);
 	}
 	setup->EvtBuilder()->SetProcName(evtBuilder.Data());
@@ -13,7 +14,7 @@
 	setup->SetNofReadouts(1);
 	TString ppcPath = gEnv->GetValue("TMbsSetup.WorkingDir", "");
 	if (ppcPath.IsNull()) {
-		cerr << setred << "mbssetup.C: Working directory missing: Please define \"TMbsSetup.WorkingDir\" in .rootrc (\"ppc\" recommended)" << endl;
+		cerr << setred << "mbssetup.C: Working directory missing: Please define \"TMbsSetup.WorkingDir\" in .rootrc (\"ppc\" recommended)" << setblack << endl;
 		gSystem->Exit(0);
 	}
 	setup->SetPath(ppcPath.Data());
