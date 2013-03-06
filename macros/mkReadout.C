e1b96c023cffe35cd8928205a0aceeed49135d58
Rudolf.Lutter <Rudolf.Lutter@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2013-03-06 11:51:55 +0000

    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@2326 514c3d59-69d9-4452-b10b-b80f2e82d9e4

---
+++ macros/mkReadout.C
@@ -0,0 +1,26 @@
+{
+	const char setred[]      =   "\033[31m";
+	const char setblack[]    =   "\033[39m";
+	ofstream of;
+	of.open("ppc.mk", ios::out);
+	of << "#!/bin/tcsh" << endl;
+	of << "source /etc/csh.login" << endl;
+	of << "source .login" << endl;
+	TString cwd = gSystem->WorkingDirectory();
+	of << "cd " << cwd << "/ppc" << endl;
+	of << "make -f *Readout.mk clean all" << endl;
+	of.close();
+	gSystem->Exec("chmod +x ppc.mk");
+	TString ppc = gEnv->GetValue("TMbsSetup.DefaultHost", "");
+	if (ppc.IsNull()) {
+		cerr << setred << "mkReadout.C: PPC not defined: Please set \"TMbsSetup.DefaultHost\" in .rootrc" << setblack << endl;
+		gSystem->Exit(1);
+	}
+	TString cmd = "rsh ";
+	cmd += ppc;
+	cmd += " ";
+	cmd += cwd;
+	cmd += "/ppc.mk";
+	gSystem->Exec(cmd);
+	gSystem->Exit(0);
+}
