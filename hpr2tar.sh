589a6d2607138729e97c73488f61054007652d0b
Otto.Schaile <Otto.Schaile@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2013-09-17 08:36:26 +0000

    update
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@2407 514c3d59-69d9-4452-b10b-b80f2e82d9e4

--- hpr2tar.sh
+++ hpr2tar.sh
@@ -1,3 +1,9 @@
-tar --exclude hpr/src/G_*.cxx -czf hpr.tgz hpr/src/*.cxx hpr/inc/*.h
-echo "List of hpr.tgz -------"
-tar tzf hpr.tgz
+tar  --exclude helpbrowser/src/G_*.cxx --exclude hpr/src/G_*.cxx   \
+--exclude fitcal/src/G_*.cxx  --exclude  gredit/src/G_*.cxx  --exclude gutils/src/G_*.cxx \
+ -czf hpr.tgz Makefile hpr/Module.mk hpr/src/*.cxx hpr/inc/*.h \
+fitcal/Module.mk fitcal/src/*.cxx fitcal/inc/*.h \
+gredit/Module.mk gredit/src/*.cxx  gredit/inc/*.h  utils/Module.mk utils/src/*.cxx utils/inc/*.h \
+gutils/Module.mk gutils/src/*.cxx gutils/inc/*.h icons/*.xpm \
+macros/*.C doc/hpr/* helpbrowser/src/*.cxx helpbrowser/inc/*.h
+echo "List of hpr.tgz ------"
+tar -tzf hpr.tgz
