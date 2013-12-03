442965f4f5be3b1bf7e8a31c9c258994dbcac56a
Rudolf.Lutter <Rudolf.Lutter@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2013-12-03 07:34:50 +0000

    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@2459 514c3d59-69d9-4452-b10b-b80f2e82d9e4

--- motCtrl/motLibs.C
+++
@@ -1,7 +0,0 @@
-{
-	gROOT->Macro("LoadUtilityLibs.C");
-	gROOT->Macro("LoadConfigLibs.C");
-	gROOT->Macro("LoadMbsLibs.C");
-	cout << "[Loading STEPPER library]" << endl;
-	gSystem->Load("libStepper.so");
-}
