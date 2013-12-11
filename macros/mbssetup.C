6429de4a66006c41a398fd989a1264aca30cac1a
Rudolf.Lutter <Rudolf.Lutter@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2013-12-11 12:25:05 +0000

    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@2468 514c3d59-69d9-4452-b10b-b80f2e82d9e4

--- macros/mbssetup.C
+++ macros/mbssetup.C
@@ -6,29 +6,47 @@
 	setup = new TMbsSetup(".mbssetup");
 	TString evtBuilder = gEnv->GetValue("TMbsSetup.EventBuilder.Name", "");
 	if (evtBuilder.IsNull()) {
-		evtBuilder = gEnv->GetValue("TMbsSetup.DefaultHost", "");
+		evtBuilder = gEnv->GetValue("TMbsSetup.PPCName", "");
 		if (evtBuilder.IsNull()) {
-			cerr << setred << "mbssetup.C: Name of eventBuilder missing: Please define \"TMbsSetup.EventBuilder.Name\" in .rootrc" << setblack << endl;
+			cerr << setred << "mbssetup.C: Name of ppc/eventBuilder missing: Please define \"TMbsSetup.PPCName\" or \"TMbsSetup.EventBuilder.Name\"  in .rootrc" << setblack << endl;
 			gSystem->Exit(1);
 		}
 	}
 	setup->EvtBuilder()->SetProcName(evtBuilder.Data());
-	TString remoteHome = setup->RemoteHomeDir();
 	setup->SetNofReadouts(1);
-	TString ppcPath = gEnv->GetValue("TMbsSetup.WorkingDir", "");
+	TString cwd = gSystem->WorkingDirectory();
+	TString ppcPath = gEnv->GetValue("TMbsSetup.PPCWorkingDir", "");
 	if (ppcPath.IsNull()) {
-		ppcPath = gEnv->GetValue("TMbsSetup.HomeDir", "");		
-		if (ppcPath.IsNull()) {
-			cerr << setred << "mbssetup.C: Working directory missing: Please define \"TMbsSetup.PPCWorkingDir\" in .rootrc (\"ppc\" recommended)" << setblack << endl;
-			gSystem->Exit(1);
-		}
+		cerr << setred << "mbssetup.C: PPC's working directory missing: Please define \"TMbsSetup.PPCWorkingDir\" in .rootrc (\"ppc\" recommended)" << setblack << endl;
+		gSystem->Exit(1);
+	}
+	if (!ppcPath.BeginsWith("/")) {
+		TString p = cwd;
+		p += "/";
+		p += ppcPath;
+		ppcPath = p;
 	}
 	setup->SetPath(ppcPath.Data());
 	TString rdoProc = gEnv->GetValue("TMbsSetup.Readout0.Name", "");
 	if (rdoProc.IsNull()) rdoProc = evtBuilder;
 	setup->ReadoutProc(0)->SetProcName(rdoProc.Data());
+	TString rdoCtrl = gEnv->GetValue("TMbsSetup.Readout0.Controller", "");
+	setup->ReadoutProc(0)->SetController(rdoCtrl.Data());
 	setup->SetMode(1);
 	setup->ReadoutProc(0)->TriggerModule()->SetType(2);
+	TString mbsVersion = gEnv->GetValue("TMbsSetup.MbsVersion", "");
+	if (mbsVersion.IsNull()) {
+		cerr << setred << "mbssetup.C: MBS version missing: Please define \"TMbsSetup.MbsVersion\" in .rootrc" << setblack << endl;
+		gSystem->Exit(1);
+	}
+	TString lynxVersion = gEnv->GetValue("TMbsSetup.LynxVersion", "");
+	if (lynxVersion.IsNull()) {
+		cerr << setred << "mbssetup.C: LynxOs version missing: Please define \"TMbsSetup.LynxVersion\" in .rootrc" << setblack << endl;
+		gSystem->Exit(1);
+	}
+	setup->Set("MbsVersion", mbsVersion.Data());
+	setup->Set("LynxVersion", lynxVersion.Data());
+
 	Bool_t ok = setup->MakeSetupFiles();
 	if (ok) setup->Save();
 	gSystem->Exit(0);
