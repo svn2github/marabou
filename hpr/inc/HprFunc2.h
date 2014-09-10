03a4d7627e87cd9ebcea6cfabc45991612c58ddf
Otto.Schaile <Otto.Schaile@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2014-09-10 11:19:34 +0000

    add  HprFunc2
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@2565 514c3d59-69d9-4452-b10b-b80f2e82d9e4

---
+++ hpr/inc/HprFunc2.h
@@ -0,0 +1,36 @@
+#ifndef HPRFUNC2
+#define HPRFUNC2
+#include <TVirtualPad.h>
+#include <TGWindow.h>
+#include <HTCanvas.h>
+#include <TRootCanvas.h>
+#include <TF2.h>
+#include <TGMenu.h>
+#include <iostream>
+//#include "TGMrbValuesAndText.h"
+//_____________________________________________________________________________________
+
+
+namespace std {} using namespace std;
+
+class TGMrbValuesAndText;
+
+class HprFunc2 : public TNamed {
+
+private:
+	HTCanvas    *fCanvas;
+	TF2         *fFunc2;
+	TGPopupMenu *fMenu;
+	TRootCanvas *fRootCanvas;
+	TString fDrawOpt;
+public:
+	HprFunc2(TF2 * f2);
+	HprFunc2(){};
+	~HprFunc2();
+	void   RecursiveRemove(TObject *obj) {};
+	void   BuildMenu();
+	void   HandleMenu(Int_t id);
+	void   SetOptions(){};
+ClassDef(HprFunc2,0)
+};
+#endif
---
+++ hpr/src/HprFunc2.cxx
@@ -0,0 +1,111 @@
+#include <TROOT.h>
+#include <TEnv.h>
+#include <TStyle.h>
+#include "SetColor.h"
+#include "TGMrbValuesAndText.h"
+#include "Set2DimOptDialog.h"
+#include "SetColorModeDialog.h"
+#include "SetCanvasAttDialog.h"
+#include "WindowSizeDialog.h"
+#include "Fit2DimDialog.h"
+
+#include "HprFunc2.h"
+
+ClassImp (HprFunc2)
+
+enum EGoHCommandIds {
+	M_Option2Dim,
+	M_Option2DimCol,
+	M_DrawFit
+};
+//________________________________________________________________________
+
+HprFunc2::HprFunc2(TF2 *f2)
+				: fFunc2(f2)
+{
+//	static const Char_t helptext[] =
+//	"Handle 2 dim functions";
+	
+	//   cout << " ctor HprFunc2::" << this << endl;
+	fCanvas = NULL;
+//   TH1* hist = 0;
+	if (fFunc2== 0) {
+		cout << setred << "No Function" << setblack << endl;
+		return;
+
+	} 		if (WindowSizeDialog::fNwindows>0) {       // not the 1. time
+			if (WindowSizeDialog::WindowSizeDialog::fWinshiftx != 0 && WindowSizeDialog::WindowSizeDialog::fNwindows%2 != 0) WindowSizeDialog::WindowSizeDialog::fWincurx += WindowSizeDialog::fWinshiftx;
+			else   {WindowSizeDialog::fWincurx = WindowSizeDialog::fWintopx; WindowSizeDialog::fWincury += WindowSizeDialog::fWinshifty;}
+		}
+		WindowSizeDialog::fNwindows++;
+		TString name=fFunc2->GetName();
+		TEnv env(".hprrc");
+		TString opt = env.GetValue("Set2DimOptDialog.fDrawOpt2Dim", "SURF2");
+		if ( opt.Contains("GL") )
+			gStyle->SetCanvasPreferGL();
+		fCanvas = new HTCanvas(name,name, WindowSizeDialog::fWincurx, WindowSizeDialog::fWincury,
+				WindowSizeDialog::fWinwidx_2dim, WindowSizeDialog::fWinwidy_2dim);
+//      fCanvasList->Add(ccc);
+		BuildMenu();
+		fFunc2->Draw(opt);
+		fCanvas->Modified();
+		fCanvas->Update();
+		cout << "Parameter values of: " << name << endl;
+		for (Int_t i = 0; i < fFunc2->GetNpar(); i++) {
+			cout << fFunc2->GetParName(i) << ": " << fFunc2->GetParameter(i) << endl;
+		}
+		cout << "-----------------------------------------------" << endl;
+}
+		
+//________________________________________________________________________
+
+HprFunc2::~HprFunc2()
+{
+//   cout <<"dtor HprFunc2, fDialog " << fDialog << endl;
+}
+//________________________________________________________________________
+
+void HprFunc2::BuildMenu()
+{
+//   cout << "HprFunc2::BuildMenu() " <<this << endl;
+	fRootCanvas = (TRootCanvas*)fCanvas->GetCanvas()->GetCanvasImp();
+	TGMenuBar * menubar = fRootCanvas->GetMenuBar();
+	TGLayoutHints * layoh_left = new TGLayoutHints(kLHintsTop | kLHintsLeft);
+	fMenu     = new TGPopupMenu(fRootCanvas->GetParent());
+	menubar->AddPopup("Display", fMenu, layoh_left, menubar->GetPopup("Help"));
+	fMenu->AddEntry("How to display a 2 dim function", M_Option2Dim);
+	fMenu->AddEntry("Color Palette selection", M_Option2DimCol);
+	fMenu->AddEntry("Change params, Redraw", M_DrawFit);
+	
+	fMenu->Connect("Activated(Int_t)", "HprFunc2", this,
+								"HandleMenu(Int_t)");
+	menubar->MapSubwindows();
+	menubar->Layout();
+}
+//________________________________________________________________________
+
+void HprFunc2::HandleMenu(Int_t id)
+{
+	switch (id) {
+
+//		case M_OptionHist:
+//			if ( GeneralAttDialog::fStackedPads ) {
+//				new SetHistOptDialog(fRootCanvas, fStack->GetHists());
+//			} else {
+//				TList *temp = new TList();
+//				temp->Add(fStack->GetHistogram());
+//				new SetHistOptDialog(fRootCanvas, temp);
+//			}
+		case M_Option2Dim:
+			new Set2DimOptDialog(fRootCanvas);
+			break;
+		case M_Option2DimCol:
+			new SetColorModeDialog(fRootCanvas);
+			break;
+		case M_DrawFit:
+//			cout << "  HprFunc2::new Fit2DimDialog(fFunc2);" << endl;
+			new Fit2DimDialog(fFunc2);
+			break;
+	}
+}
+
