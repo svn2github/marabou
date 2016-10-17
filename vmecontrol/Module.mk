8aa0e971248c82346cb9151da4b55b0c4996ebd5
Otto.Schaile <Otto.Schaile@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2016-10-17 11:18:31 +0000

    explicitlink, root 6
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@2773 514c3d59-69d9-4452-b10b-b80f2e82d9e4

--- vmecontrol/Module.mk
+++ vmecontrol/Module.mk
@@ -2,6 +2,7 @@
 # Copyright (c) 2000 Rene Brun and Fons Rademakers
 #
 # Author: Otto Schaile, 29/2/2000
+#			13/7/2016 comment away libVMEControl.so
 
 MODDIR       := vmecontrol
 MODDIRS      := $(MODDIR)/src
@@ -14,9 +15,10 @@ VMECDIRI     := $(VMECDIR)/inc
 ##### lib #####
 VMECL        := $(MODDIRI)/LinkDef.h
 VMECDS       := $(MODDIRS)/G__VMEControlDict.cxx
-VMECPCM       := $(MODDIRS)/G__VMEControlDict_rdict.pcm
+VMECPCM      := $(MODDIRS)/G__VMEControlDict_rdict.pcm
 VMECDO       := $(VMECDS:.cxx=.o)
-VMECH        := $(filter-out $(MODDIRI)/LinkDef%,$(wildcard $(MODDIRI)/*.h))
+VMECHL       := $(filter-out $(MODDIRI)/LinkDef%,$(wildcard $(MODDIRI)/*.h))
+VMECH        :=  $(patsubst $(MODDIRI)/%.h,%.h,$(VMECHL))
 VMECS        := $(filter-out $(MODDIRS)/G__%,$(wildcard $(MODDIRS)/*.cxx))
 VMECOM       := $(VMECS:.cxx=.o)
 VMECMAINO    := $(MODDIRS)/VMEControl.o
@@ -27,7 +29,7 @@ VMECDH			:= $(VMECH)
 VMECDEP      := $(VMECO:.o=.d) $(VMECDO:.o=.d)
 
 VMECEXE      := bin/VMEControl
-VMECLIB       := $(LPATH)/libVMEControl.so
+#VMECLIB       := $(LPATH)/libVMEControl.so
 
 ALLEXECS    += $(VMECEXE)
 
@@ -36,7 +38,7 @@ ifeq ($(ROOTV6), 1)
 	ALLPCMS += $(VMECPCM)
 endif
 # used in the vmeControl Makefile
-ALLHDRS     += $(patsubst $(MODDIRI)/%.h,include/%.h,$(VMECH))
+ALLHDRS     += $(patsubst $(MODDIRI)/%.h,include/%.h,$(VMECHL))
 
 # include all dependency files
 INCLUDEFILES += $(VMECDEP)
@@ -49,28 +51,31 @@ OVMECLIBS      := $(LPATH)/libTMrbC2Lynx.$(SOEXT) $(LPATH)/libTMrbUtils.$(SOEXT)
 include/%.h:    $(VMECDIRI)/%.h
 		cp $< $@
 
-$(VMECEXE):     $(OVMECLIBS) $(VMECSO)$(VMECO) $(VMECMAINO)
+$(VMECEXE): $(OVMECLIBS) $(VMECDO) $(VMECO) $(VMECMAINO)
 #		@echo "sources: $(VMECS)"
-		@echo "OVMECLIBS: $(OVMECLIBS)"
 		@echo "$(VMECEXE) linking exe ----------------------------------"
 		$(LD) -g $(LDFLAGS) $(VMECMAINO) $(VMECO) $(VMECDO) $(OVMECLIBS) $(ROOTGLIBS) -lSpectrum \
             -o $(VMECEXE)
 
-$(VMECLIB):     $(VMECDO) $(VMECO)
-		@echo "objs in libVMEControl.so: $(VMECO)"
+#$(VMECLIB):     $(VMECDO) $(VMECO)
+#		@echo "objs in libVMEControl.so: $(VMECO)"
 #		@echo "objs: $(VMECO)"
-		@echo "$(VMECEXE) make shared lib ------------------------------------"
-		@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" \
-		   "$(SOFLAGS)" libVMEControl.$(SOEXT) $@ "$(VMECO) $(VMECDO)"
-		@(if [ -f $(VMECPCM) ] ; then \
-			echo "cp  $(VMECPCM)----------------------" ; \
-			cp $(VMECPCM) $(LPATH); \
-		fi)
+#		@echo "$(VMECEXE) make shared lib ------------------------------------"
+#		@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" \
+#		   "$(SOFLAGS)" libVMEControl.$(SOEXT) $@ "$(VMECO) $(VMECDO)"
+#		@(if [ -f $(VMECPCM) ] ; then \
+#			echo "cp  $(VMECPCM)----------------------" ; \
+#			cp $(VMECPCM) $(LPATH); \
+#		fi)
 
-$(VMECDS):     $(VMECDH) $(VMECL)
-		@echo "includes: $(VMECDH)"
-		@echo "Generating dictionary $@..."
-		$(ROOTCINT) -f $@ -c -p -Iinclude $(VMECDH) $(VMECL)
+$(VMECDS):     $(VMECHL) $(VMECL)
+#		@echo "includes: $(VMECDH)"
+		@echo "Generating dictionary $@... for ROOT $(ROOT_MAJOR)"
+ifneq ($(ROOTV6), 1)
+		$(ROOTCINT) -f $@ -c -p -Iinclude $(VMECH) $(VMECL)
+else
+		rootcling -f $@ $(call dictModule,VMEC) -I$(MARABOU_SRCDIR)/include $(VMECH) $(VMECL)
+endif
 
 $(VMECDO):     $(VMECDS)
 		$(CXX) $(NOOPT) $(CXXFLAGS) -I. -o $@ -c $<
