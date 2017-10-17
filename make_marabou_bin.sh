fde2a92c50cad28f1bd9c032c4b4770e3ab9a6a6
Otto.Schaile <Otto.Schaile@514c3d59-69d9-4452-b10b-b80f2e82d9e4>
2017-10-17 07:59:59 +0000

    remove
    git-svn-id: https://svn.physik.uni-muenchen.de/repos/marabou/trunk/marabou@2859 514c3d59-69d9-4452-b10b-b80f2e82d9e4

--- make_marabou_bin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rootversion=`root-config --version | sed 's%/%.%'`
-tarfile=marabou_$rootversion.tgz
-echo $tarfile
-tar -czf $tarfile bin data  icons  include  lib  macros  obj  powerpc  sounds  templates
-
